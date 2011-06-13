718012bf5e252407e7a4b3d59084230f1d33bde2
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2011-06-13 21:37:16 +0000

    Added CRM pool
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@24 02885c34-e171-44ab-8435-42fb7a3c3aaf

---
+++ TeamSupport.CrmIntegration/CrmPool.vb
@@ -0,0 +1,75 @@
+﻿Imports TeamSupport.Data
+Imports TeamSupport.ServiceLibrary
+Imports System.IO
+
+Namespace TeamSupport
+  Namespace CrmIntegration
+
+    Public Class CrmPool
+      Inherits ServiceThread
+
+
+      Public Overrides ReadOnly Property ServiceName As String
+        Get
+          Return "CrmPool"
+        End Get
+      End Property
+
+      Public Overrides Sub Run()
+        Dim maxThreads As Integer = Settings.ReadInt("Max Worker Processes", 1)
+        Dim threads As New List(Of CrmProcessor)()
+
+        'Check if stopped, if so spread the word
+        If IsStopped Then
+          For Each thread As CrmProcessor In threads
+            If Not thread.IsStopped Then
+              thread.Stop()
+            End If
+          Next
+          Return
+        End If
+
+
+        'Remove completed threads
+        For Each thread As CrmProcessor In threads
+          If thread.IsComplete Then
+            threads.Remove(thread)
+          End If
+        Next
+
+        'See if we have reached our max thread coutn
+        If threads.Count >= maxThreads Then
+          Return
+        End If
+
+        'Load the waiting links to process
+        Dim links As New CRMLinkTable(_loginUser)
+        links.LoadActive()
+
+        For Each link As CRMLinkTableItem In links
+          If Not IsAlreadyProcessing(threads, link.OrganizationID) Then
+            Dim crmProcessor As New CrmProcessor(link.OrganizationID)
+            crmProcessor.Start()
+            threads.Add(crmProcessor)
+            Return
+          End If
+        Next
+      End Sub
+
+
+      Private Function IsAlreadyProcessing(ByRef threads As List(Of CrmProcessor), ByVal organizationID As Integer) As Boolean
+        For Each thread As CrmProcessor In threads
+          If thread.OrganizationID = organizationID Then
+            Return True
+          End If
+        Next
+        Return False
+      End Function
+
+
+    End Class
+
+  End Namespace
+End Namespace
+
+
--- TeamSupport.CrmIntegration/CrmProcessor.vb
+++ TeamSupport.CrmIntegration/CrmProcessor.vb
@@ -3,96 +3,81 @@ Imports TeamSupport.ServiceLibrary
 Imports System.IO
 
 Namespace TeamSupport
-    Namespace CrmIntegration
+  Namespace CrmIntegration
 
-        Public Class CrmProcessor
-            Inherits ServiceThread
+    Public Class CrmProcessor
+      Inherits ServiceThread
 
+      Public Overrides ReadOnly Property ServiceName As String
+        Get
+          Return "CrmProcessor"
+        End Get
+      End Property
 
+      Private _organizationID As Integer
+      Public ReadOnly Property OrganizationID() As Integer
+        Get
+          Dim result As Integer
+          SyncLock Me
+            result = _organizationID
+          End SyncLock
+          Return result
+        End Get
+      End Property
 
-            ''' <summary>
-            ''' This function is called by the service to run a specified interval.  This function is called in its own thread.
-            ''' </summary>
-            ''' <remarks>Check the property IsStopped to see if you need to stop processing.</remarks>
-            Public Overrides Sub Run()
-                Dim crmLinkTable As New CRMLinkTable(LoginUser)
-                'This loads a DataTable with the Active CRMLinkTable records
-                crmLinkTable.LoadActive()
+      Public Sub New(ByVal organizationID As Integer)
+        _organizationID = organizationID
+        IsLoop = False
+      End Sub
 
+      Public Overrides Sub Run()
+        Dim crmLinkTable As New CRMLinkTable(LoginUser)
+        crmLinkTable.LoadByOrganizationID(OrganizationID)
+        If Not crmLinkTable.IsEmpty Then
 
+          Try
+            ProcessOrganization(crmLinkTable(0))
 
-                'You can iterate through the table
-                For Each crmLinkTableItem As CRMLinkTableItem In crmLinkTable
-                    Try
+          Catch ex As Exception
+            ex.Data("OrganizationID") = OrganizationID
+            ExceptionLogs.LogException(LoginUser, ex, "Service - " & ServiceName, crmLinkTable(0).Row)
+          End Try
 
-                        If IsStopped Then
-                            Return
-                        End If
-
-
-                        ProcessOrganization(crmLinkTableItem)
-
-
-
-                        'You can update a record like this
-                        crmLinkTableItem.LastLink = DateTime.UtcNow
-                        'This saves the whole table, not just this record.  It can be called at the end of the loop on the outside as well
-                        crmLinkTableItem.Collection.Save()
-
-                        'There is a bug currently to add a new record to an already loaded datatable (i have just never fixed it)
-                        'You can do it in this way
-                        'Dim newItem As CRMLinkTableItem = (New CRMLinkTable(LoginUser)).AddNewCRMLinkTableItem()
-                        'newItem.Active = True
-                        'newItem.OrganizationID = 1234
-                        ''set the rest of the properties here
-                        'newItem.Collection.Save()
-
-
-
-                    Catch ex As Exception
-                        'This is an example of how to log exceptions into the ExceptionLogs table
-
-                        'This will store extra data in the exception and write it to the table
-                        'You can see that we can access the OrganizationID field as a property
-                        ex.Data("OrganizationID") = crmLinkTableItem.OrganizationID
-
-                        'This will log the exception and the Row of the DataTable
-                        ExceptionLogs.LogException(LoginUser, ex, "Service - " & ServiceName, crmLinkTableItem.Row)
-                    End Try
+          crmLinkTable(0).LastProcessed = DateTime.UtcNow
+          crmLinkTable.Save()
+        End If
 
 
-                Next
-
-            End Sub
+      End Sub
 
 
-            Public Sub ProcessOrganization(ByVal crmLinkTableItem As CRMLinkTableItem)
-                Dim CRMType As IntegrationType = [Enum].Parse(GetType(IntegrationType), crmLinkTableItem.CRMType)
+      Public Sub ProcessOrganization(ByVal crmLinkTableItem As CRMLinkTableItem)
+        Dim CRMType As IntegrationType = [Enum].Parse(GetType(IntegrationType), crmLinkTableItem.CRMType)
 
-                'set up log per organization
-                Dim Log As New SyncLog(Path.Combine(Settings.ReadString("Log File Path", "C:\CrmLogs\"), crmLinkTableItem.OrganizationID.ToString()))
-                Dim CRM As Integration
+        'set up log per organization
+        Dim Log As New SyncLog(Path.Combine(Settings.ReadString("Log File Path", "C:\CrmLogs\"), crmLinkTableItem.OrganizationID.ToString()))
+        Dim CRM As Integration
 
-                Select Case CRMType
-                    Case IntegrationType.Batchbook
-                        CRM = New BatchBook(crmLinkTableItem, Log, LoginUser)
-                        CRM.PerformSync()
-                        CRM.SendTicketData()
-                    Case IntegrationType.Highrise
-                    Case IntegrationType.SalesForce
-                End Select
-            End Sub
+        Select Case CRMType
+          Case IntegrationType.Batchbook
+            CRM = New BatchBook(crmLinkTableItem, Log, LoginUser)
+            CRM.PerformSync()
+            CRM.SendTicketData()
+          Case IntegrationType.Highrise
+          Case IntegrationType.SalesForce
+        End Select
+      End Sub
 
-        End Class
+    End Class
 
-        'TODO: maybe we move this into teamsupport.data?
-        Public Enum IntegrationType
-            SalesForce
-            Highrise
-            Batchbook
-            FreshBooks
-        End Enum
-    End Namespace
+    'TODO: maybe we move this into teamsupport.data?
+    Public Enum IntegrationType
+      SalesForce
+      Highrise
+      Batchbook
+      FreshBooks
+    End Enum
+  End Namespace
 End Namespace
 
 
