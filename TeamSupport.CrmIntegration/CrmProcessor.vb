aa9b2db405e966e0b7db5757ff43dba1e62a20c9
alexisaurus <alexisaurus@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2011-06-16 21:16:18 +0000

    log success to crmlinkresults table
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@47 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.CrmIntegration/CrmProcessor.vb
+++ TeamSupport.CrmIntegration/CrmProcessor.vb
@@ -3,104 +3,112 @@ Imports TeamSupport.ServiceLibrary
 Imports System.IO
 
 Namespace TeamSupport
-  Namespace CrmIntegration
+    Namespace CrmIntegration
 
-    Public Class CrmProcessor
-      Inherits ServiceThread
+        Public Class CrmProcessor
+            Inherits ServiceThread
 
-      Public Overrides ReadOnly Property ServiceName As String
-        Get
-          Return "CrmProcessor"
-        End Get
-      End Property
+            Public Overrides ReadOnly Property ServiceName As String
+                Get
+                    Return "CrmProcessor"
+                End Get
+            End Property
 
-      Public Overridable ReadOnly Property IsStopped() As Boolean
-        Get
-          SyncLock Me
-            Return _isStopped Or ServiceStopped Or CrmPool.CrmPoolStopped
-          End SyncLock
-        End Get
-      End Property
+            Public Overridable ReadOnly Property IsStopped() As Boolean
+                Get
+                    SyncLock Me
+                        Return _isStopped Or ServiceStopped Or CrmPool.CrmPoolStopped
+                    End SyncLock
+                End Get
+            End Property
 
       Private _crmLinkID As Integer
       Public ReadOnly Property CrmLinkID() As Integer
-        Get
-          Dim result As Integer
-          SyncLock Me
+                Get
+                    Dim result As Integer
+                    SyncLock Me
             result = _crmLinkID
-          End SyncLock
-          Return result
-        End Get
-      End Property
+                    End SyncLock
+                    Return result
+                End Get
+            End Property
 
       Public Sub New(ByVal cmrLinkId As Integer)
         _crmLinkID = cmrLinkId
-        IsLoop = False
-      End Sub
+                IsLoop = False
+            End Sub
 
-      Public Overrides Sub Run()
+            Public Overrides Sub Run()
         Dim CrmLinkRow As CRMLinkTableItem = CRMLinkTable.GetCRMLinkTableItem(LoginUser, _crmLinkID)
 
-        Try
+                Try
           ProcessCrmLink(CrmLinkRow)
-          'System.Threading.Thread.Sleep(5000)
+                    'System.Threading.Thread.Sleep(5000)
 
-        Catch ex As Exception
+                Catch ex As Exception
           ex.Data("CRMLinkID") = _crmLinkID
-          ExceptionLogs.LogException(LoginUser, ex, "Service - " & ServiceName, CrmLinkRow.Row)
-        End Try
+                    ExceptionLogs.LogException(LoginUser, ex, "Service - " & ServiceName, CrmLinkRow.Row)
+                End Try
 
-        CrmLinkRow.LastProcessed = DateTime.UtcNow
-        CrmLinkRow.Collection.Save()
+                CrmLinkRow.LastProcessed = DateTime.UtcNow
+                CrmLinkRow.Collection.Save()
 
-      End Sub
+            End Sub
 
 
       Public Sub ProcessCrmLink(ByVal CRMLinkTableItem As CRMLinkTableItem)
-        Dim Success As Boolean = True
+                Dim Success As Boolean = True
 
-        If IsStopped Then
-          Return
-        End If
+                If IsStopped Then
+                    Return
+                End If
 
         Dim CRMType As IntegrationType = [Enum].Parse(GetType(IntegrationType), CRMLinkTableItem.CRMType, True)
 
         'set up log per crm link item
         Dim Log As New SyncLog(Path.Combine(Settings.ReadString("Log File Path", "C:\CrmLogs\"), CRMLinkTableItem.OrganizationID.ToString()))
-        Dim CRM As Integration
+                Dim CRM As Integration
 
-        Select Case CRMType
-          Case IntegrationType.Batchbook
+                Select Case CRMType
+                    Case IntegrationType.Batchbook
             CRM = New BatchBook(CRMLinkTableItem, Log, LoginUser, Me)
-          Case IntegrationType.Highrise
+                    Case IntegrationType.Highrise
             CRM = New Highrise(CRMLinkTableItem, Log, LoginUser, Me)
-          Case IntegrationType.SalesForce
-        End Select
+                    Case IntegrationType.SalesForce
+                End Select
 
-        'only process bb and hr
-        If CRMType = IntegrationType.Batchbook Or CRMType = IntegrationType.Highrise Then
-          Log.Write(String.Format("Begin processing {0} sync.", CRMType.ToString()))
+                'only process bb and hr
+                If CRMType = IntegrationType.Batchbook Or CRMType = IntegrationType.Highrise Then
+                    Log.Write(String.Format("Begin processing {0} sync.", CRMType.ToString()))
 
-          Success = CRM.PerformSync()
+                    Success = CRM.PerformSync()
 
-          If Success Then
-            Success = CRM.SendTicketData()
-          End If
+                    If Success Then
+                        Success = CRM.SendTicketData()
+                    End If
 
-          If Success Then
+                    If Success Then
             CRMLinkTableItem.LastLink = DateTime.UtcNow
             CRMLinkTableItem.Collection.Save()
-            Log.Write("Finished processing successfully.")
-          Else
-            Log.Write(String.Format("Error reported in {0} sync. Last link date/time not updated.", CRMType.ToString()))
-          End If
+                        Log.Write("Finished processing successfully.")
 
-        End If
-      End Sub
+                        Dim result As CRMLinkResult
+                        result = (New CRMLinkResults(LoginUser)).AddNewCRMLinkResult()
+                        result.AttemptResult = "Completed"
+                        result.OrganizationID = CrmLinkRow.OrganizationID
+                        result.AttemptDateTime = Now.ToUniversalTime()
+                        result.Collection.Save()
 
-    End Class
+                    Else
+                        Log.Write(String.Format("Error reported in {0} sync. Last link date/time not updated.", CRMType.ToString()))
+                    End If
 
-  End Namespace
+                End If
+            End Sub
+
+        End Class
+
+    End Namespace
 End Namespace
 
 
