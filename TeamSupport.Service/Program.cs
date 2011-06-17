d33989b4a781048910126ee92a129d2a090b6bd6
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2011-06-17 19:27:45 +0000

    Updated the way the service installer works for the EdgeCast install.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@55 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.Service/Program.cs
+++ TeamSupport.Service/Program.cs
@@ -3,22 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
+using TeamSupport.Data;
+using TeamSupport.ServiceLibrary;
+using TeamSupport.CrmIntegration;
+using Microsoft.Win32;
+using System.Data;
+using System.Diagnostics;
+
 
 namespace TeamSupport.Service
 {
-  static class Program
+  class Program : ServiceBase
   {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    static void Main()
+    List<ServiceThread> _threads;
+
+    public Program()  
     {
-      ServiceBase[] ServicesToRun;
-      ServicesToRun = new ServiceBase[] 
-			{ 
-				new Service() 
-			};
-      ServiceBase.Run(ServicesToRun);
+      this.ServiceName = "TeamSupport";
+      _threads = new List<ServiceThread>();
+      _threads.Add(new EmailProcessor());
+      _threads.Add(new EmailSender());
+      _threads.Add(new SlaProcessor());
+      _threads.Add(new Indexer());
+      _threads.Add(new CrmPool());
     }
+
+    static void Main(string[] args)
+    {
+      ServiceBase.Run(new Program());
+    }
+
+    protected override void OnStart(string[] args)
+    {
+      System.Diagnostics.Process.GetCurrentProcess().PriorityClass = System.Diagnostics.ProcessPriorityClass.Idle;
+      StartProcessing();
+    }
+
+    protected override void OnStop()
+    {
+      ServiceThread.ServiceStopped = true;
+      StopProcessing();
+    }
+
+    protected override void OnShutdown()
+    {
+      StopProcessing();
+    }
+
+    private void StopProcessing()
+    {
+      foreach (ServiceThread thread in _threads)
+      {
+        thread.Stop();
+      }
+    }
+
+    private void StartProcessing()
+    {
+      foreach (ServiceThread thread in _threads)
+      {
+        thread.Start();
+      }
+    }
+
+
   }
 }
