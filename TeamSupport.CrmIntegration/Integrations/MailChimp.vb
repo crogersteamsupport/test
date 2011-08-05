f669f214ed539d791dbe3992ea7d2b47546015c4
alexisaurus <alexisaurus@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2011-08-05 20:35:10 +0000

    major update to mailchimp sync: unsubscribe users who have been marked inactive since last sync check last modified date of both user and organization to determine who to update fixed issue with mismatch between utc/local time
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@112 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.CrmIntegration/Integrations/MailChimp.vb
+++ TeamSupport.CrmIntegration/Integrations/MailChimp.vb
@@ -36,37 +36,58 @@ Namespace TeamSupport
 
                     Dim emailBatches As New List(Of StringBuilder)()
                     Dim emailBatch As StringBuilder = Nothing
+                    Dim unsubBatch As StringBuilder = Nothing
                     Dim emailIndex As Integer = 0
+                    Dim unsubIndex As Integer = 0
 
                     For Each customer As Organization In theseCustomers
 
                         Dim theseContacts As New Users(User)
-                        theseContacts.LoadByOrganizationID(customer.OrganizationID, True)
+                        theseContacts.LoadByOrganizationID(customer.OrganizationID, False)
 
                         For Each contact As User In theseContacts
                             If Processor.IsStopped Then
                                 Return False
                             End If
 
-                            If (CRMLinkRow.LastLink Is Nothing Or contact.DateModified.AddMinutes(30) > CRMLinkRow.LastLink) And Not contact.MarkDeleted Then
-                                If emailBatch Is Nothing Then
-                                    emailBatch = New StringBuilder("&apikey=" & CRMLinkRow.SecurityToken _
-                                                                   & "&id=" & listID _
-                                                                   & "&double_optin=false")
+                            If contact.IsActive And customer.IsActive Then
+
+                                If (CRMLinkRow.LastLink Is Nothing Or CType(contact.Row("DateModified"), DateTime).AddMinutes(30) > CRMLinkRow.LastLink _
+                                    Or CType(customer.Row("DateModified"), DateTime).AddMinutes(30) > CRMLinkRow.LastLink) And Not contact.MarkDeleted Then
+                                    If emailBatch Is Nothing Then
+                                        emailBatch = New StringBuilder("&apikey=" & CRMLinkRow.SecurityToken _
+                                                                       & "&id=" & listID _
+                                                                       & "&double_optin=false")
+                                    End If
+
+                                    emailBatch.Append("&batch[" & emailIndex & "][EMAIL]=" & contact.Email)
+                                    emailBatch.Append("&batch[" & emailIndex & "][EMAIL_TYPE]=html")
+                                    emailBatch.Append("&batch[" & emailIndex & "][FNAME]=" & contact.FirstName)
+                                    emailBatch.Append("&batch[" & emailIndex & "][LNAME]=" & contact.LastName)
+                                    emailIndex += 1
                                 End If
 
-                                emailBatch.Append("&batch[" & emailIndex & "][EMAIL]=" & contact.Email)
-                                emailBatch.Append("&batch[" & emailIndex & "][EMAIL_TYPE]=html")
-                                emailBatch.Append("&batch[" & emailIndex & "][FNAME]=" & contact.FirstName)
-                                emailBatch.Append("&batch[" & emailIndex & "][LNAME]=" & contact.LastName)
-                                emailIndex += 1
-                            End If
+                                If emailIndex = BatchSize Then
+                                    emailBatches.Add(emailBatch)
 
-                            If emailIndex = BatchSize Then
-                                emailBatches.Add(emailBatch)
+                                    emailIndex = 0
+                                    emailBatch = Nothing
+                                End If
 
-                                emailIndex = 0
-                                emailBatch = Nothing
+                            Else
+                                If CRMLinkRow.LastLink Is Nothing Or CType(contact.Row("DateModified"), DateTime).AddMinutes(30) > CRMLinkRow.LastLink _
+                                    Or CType(customer.Row("DateModified"), DateTime).AddMinutes(30) > CRMLinkRow.LastLink Then
+
+                                    If unsubBatch Is Nothing Then
+                                        unsubBatch = New StringBuilder("&apikey=" & CRMLinkRow.SecurityToken _
+                                                                       & "&id=" & listID _
+                                                                       & "&send_goodbye=false")
+                                    End If
+
+                                    unsubBatch.Append("&emails[" & unsubIndex & "]=" & contact.Email)
+                                    unsubIndex += 1
+                                End If
+
                             End If
                         Next
                     Next
@@ -87,6 +108,15 @@ Namespace TeamSupport
                         Log.Write("Addresses sent to Mailchimp in " & emailBatches.Count.ToString() & " batches.")
                     Else
                         Log.Write("No new email addresses to sync.")
+                    End If
+
+                    If unsubBatch IsNot Nothing Then
+                        If Not BatchUnsubscribe(unsubBatch.ToString()) Then
+                            Log.Write("Error in BatchUnsubscribe.")
+                            Return False
+                        End If
+
+                        Log.Write("Unsubscribed " & unsubIndex.ToString() & " addresses.")
                     End If
 
                 Else
@@ -136,6 +166,12 @@ Namespace TeamSupport
                 Return PostQueryString(Nothing, MCUri, postData) = HttpStatusCode.OK
             End Function
 
+            Private Function BatchUnsubscribe(ByVal postString As String) As Boolean
+                Dim MCUri As New Uri("https://" & datacenter & ".api.mailchimp.com/1.3/?method=listBatchUnsubscribe")
+                Dim postData As String = "&output=xml" & postString
+
+                Return PostQueryString(Nothing, MCUri, postData) = HttpStatusCode.OK
+            End Function
         End Class
 
     End Namespace
