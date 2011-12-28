f303e25fbc2ccb64dc31ca21559a088d4c118e01
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2011-12-28 22:40:24 +0000

    Added basic user page.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@258 02885c34-e171-44ab-8435-42fb7a3c3aaf

---
+++ WebApp/Resources/Pages/User.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="ts/ts.js" />
+/// <reference path="ts/top.Ts.Services.js" />
+/// <reference path="ts/ts.system.js" />
+/// <reference path="ts/ts.utils.js" />
+/// <reference path="ts/ts.pages.main.js" />
+/// <reference path="~/Default.aspx" />
+
+var userPage = null;
+$(document).ready(function () {
+  userPage = new UserPage();
+  userPage.refresh();
+});
+
+function onShow() {
+  userPage.refresh();
+};
+
+
+UserPage = function () {
+  $('#btnRefresh')
+  .click(function (e) {
+    e.preventDefault();
+    window.location = window.location;
+  })
+  .toggle(window.location.hostname.indexOf('127.0.0.1') > -1);
+
+  $('button').button();
+  $('a').addClass('ui-state-default ts-link');
+  var userID = top.Ts.Utils.getQueryValue("userid", window);
+  top.Ts.Services.Users.GetUser(userID, function (user) {
+    $('.user-displayname').text(user.FirstName + ' ' + user.LastName);
+
+  });
+  $('.loading-section').hide().next().show();
+};
+
+
+UserPage.prototype = {
+  constructor: UserPage,
+  refresh: function () {
+
+  }
+};
