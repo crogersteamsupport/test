2bf516cb9e7c66195c557a270634cb64ca9dc64a
lperezpicasso <lperezpicasso@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2012-08-09 17:55:09 +0000

    [Tickets:7894][Version:1.1.01] Added Wiki index.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@496 02885c34-e171-44ab-8435-42fb7a3c3aaf

---
+++ TeamSupport.ServiceLibrary/IndexDataSource.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using TeamSupport.Data;
+
+namespace TeamSupport.ServiceLibrary
+{
+  [Serializable]
+  class IndexDataSource : dtSearch.Engine.DataSource
+  {
+    protected   int       _organizationID;
+    protected   LoginUser _loginUser = null;
+    protected   Logs      _logs;
+
+    protected   int       _maxCount;
+
+    protected   List<int> _itemIDList   = null;
+    protected   List<int> _updatedItems = null;
+    protected   int       _rowIndex     = 0;
+    protected   int?      _lastItemID   = null;
+
+    public    List<int> UpdatedItems
+    {
+      get { lock (this) { return _updatedItems; } }
+    }
+    
+    protected IndexDataSource() { }
+
+    public IndexDataSource(LoginUser loginUser, int maxCount, int organizationID)
+    {
+      _organizationID = organizationID;
+      _loginUser      = new LoginUser(loginUser.ConnectionString, loginUser.UserID, loginUser.OrganizationID, null);
+
+      _maxCount = maxCount;
+
+      _updatedItems = new List<int>();
+
+      DocIsFile       = false;
+      DocName         = "";
+      DocDisplayName  = "";
+      DocText         = "";
+      DocFields       = "";
+      DocCreatedDate  = System.DateTime.UtcNow;
+      DocModifiedDate = System.DateTime.UtcNow;
+    }
+
+    override public bool GetNextDoc()
+    {
+      return false;
+    }
+
+    override public bool Rewind()
+    {
+      return false;
+    }
+  }
+}
+
---
+++ TeamSupport.ServiceLibrary/WikiIndexDataSource.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using TeamSupport.Data;
+
+namespace TeamSupport.ServiceLibrary
+{
+  [Serializable]
+  class WikiIndexDataSource : IndexDataSource
+  {
+    protected WikiIndexDataSource() {}
+    
+    public WikiIndexDataSource(LoginUser loginUser, int maxCount, int organizationID) : base(loginUser, maxCount, organizationID)
+    {
+      _logs = new Logs("Wiki Indexer DataSource");      
+    }
+
+    override public bool GetNextDoc()
+    {
+      try
+      {
+        if (_itemIDList == null) { Rewind(); }
+        if (_lastItemID != null) { UpdatedItems.Add((int)_lastItemID); }
+        _rowIndex++;
+        if (_itemIDList.Count <= _rowIndex) { return false; }
+
+        WikiArticlesViewItem wiki = WikiArticlesView.GetWikiArticlesViewItem(_loginUser, _itemIDList[_rowIndex]);
+        _logs.WriteEvent("Started Processing Wiki ArticleID: " + wiki.ArticleID.ToString());
+
+        _lastItemID = wiki.ArticleID;
+
+        DocText = string.Format("<html><body>{0}</body></html>", wiki.Body);
+
+        DocFields = string.Empty;
+        foreach (DataColumn column in wiki.Collection.Table.Columns)
+        {
+          object value = wiki.Row[column];
+          string s = value == null || value == DBNull.Value ? "" : value.ToString();
+          DocFields += column.ColumnName + "\t" + s.Replace("\t", " ") + "\t";
+        }
+
+        DocId           = wiki.ArticleID;
+
+        DocIsFile       = false;
+        DocName         = wiki.ArticleID.ToString();
+        DocDisplayName  = wiki.ArticleName;
+        DocCreatedDate  = wiki.CreatedDate;
+        DocModifiedDate = DateTime.UtcNow;
+
+        return true;
+      }
+      catch (Exception ex)
+      {
+        ExceptionLogs.LogException(_loginUser, ex, "WikiIndexDataSource");
+        throw;
+      }
+    }
+
+    override public bool Rewind()
+    {
+      try
+      {
+      _logs.WriteEvent("Rewound wikis, OrgID: " + _organizationID.ToString());
+      _itemIDList = new List<int>();
+      WikiArticlesView wikis = new WikiArticlesView(_loginUser);
+      wikis.LoadForIndexing(_organizationID, _maxCount);
+      foreach(WikiArticlesViewItem wiki in wikis)
+      {
+        _itemIDList.Add(wiki.ArticleID);
+      }
+      _lastItemID = null;
+      _rowIndex = -1;
+      }
+      catch (Exception ex)
+      {
+        ExceptionLogs.LogException(_loginUser, ex, "WikiIndexDataSource.Rewind");
+        throw;
+      }
+
+      return true;
+    }
+  }
+}
