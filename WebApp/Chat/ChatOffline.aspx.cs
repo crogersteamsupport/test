1fe49e7b5a6d7a56807a975603465a385de451a7
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2014-01-17 23:11:12 +0000

    Changed offline chat form for AMCO [Tickets: 14751]
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@1395 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- WebApp/Chat/ChatOffline.aspx.cs
+++ WebApp/Chat/ChatOffline.aspx.cs
@@ -62,7 +62,7 @@ public partial class Chat_ChatOffline : System.Web.UI.Page
       if (_organization.OrganizationID == 566596)
       {
         pnlChatForm.Visible = false;
-        pnlCustom.InnerHtml = "<p>Lo sentimos, por el momento no hay un ejecutivo disponible. Por favor de dar click <a href=\"https://ticket.teamsupport.com/AMCO\" target=\"_blank\">aquí</a> para solicitar asistencia y nos comunicaremos con usted en un máximo de 24 horas.</p>";
+        pnlCustom.InnerHtml = "<p style=\"font-weight:bold;\">Lo sentimos, por el momento no hay un ejecutivo disponible.</p><p>Por favor de click <a href=\"https://ticket.teamsupport.com/AMCO\" target=\"_blank\">aquí</a> para solicitar asistencia y nos comunicaremos con usted en un máximo de 24 horas.</p>";
         pnlQuestion.Visible = false;
         headTitle.Visible = false;
       }
