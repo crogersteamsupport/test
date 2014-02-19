d6398c526b49ebb41eb46d35b6863f0727f4583c
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2014-02-19 22:55:13 +0000

    Updated customer search
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@1631 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.ServiceLibrary/Indexer.cs
+++ TeamSupport.ServiceLibrary/Indexer.cs
@@ -51,7 +51,7 @@ namespace TeamSupport.ServiceLibrary
             }
 
             cnt++;
-            //ProcessIndex(org, ReferenceType.Tickets, isRebuilder);
+            ProcessIndex(org, ReferenceType.Tickets, isRebuilder);
             ProcessIndex(org, ReferenceType.Wikis, isRebuilder);
             ProcessIndex(org, ReferenceType.Notes, isRebuilder);
             ProcessIndex(org, ReferenceType.ProductVersions, isRebuilder);
