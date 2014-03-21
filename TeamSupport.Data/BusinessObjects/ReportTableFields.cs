ac402ba732f53423a939091dbc4863a1f6acb13f
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2014-03-21 20:44:33 +0000

    Tweaked new editor styles
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@1827 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.Data/BusinessObjects/ReportTableFields.cs
+++ TeamSupport.Data/BusinessObjects/ReportTableFields.cs
@@ -18,7 +18,8 @@ namespace TeamSupport.Data
     {
       using (SqlCommand command = new SqlCommand())
       {
-        command.CommandText = "SELECT rtf.*, rt.Alias AS TableAlias FROM ReportTableFields rtf LEFT JOIN ReportTables rt ON rt.ReportTableID = rtf.ReportTableID WHERE IsVisible = 1 ORDER BY rtf.Alias";
+        command.CommandText = "SELECT rtf.*, rt.Alias AS TableAlias FROM ReportTableFields rtf LEFT JOIN ReportTables rt ON rt.ReportTableID = rtf.ReportTableID WHERE (IsVisible = 1 OR @OrganizationID=1078) ORDER BY rtf.Alias";
+        command.Parameters.AddWithValue("OrganizationID", LoginUser.OrganizationID);
         command.CommandType = CommandType.Text;
         Fill(command);
       }
@@ -28,10 +29,11 @@ namespace TeamSupport.Data
     {
       using (SqlCommand command = new SqlCommand())
       {
-        command.CommandText = "SELECT rtf.*, rt.Alias AS TableAlias FROM ReportTableFields rtf LEFT JOIN ReportTables rt ON rt.ReportTableID = rtf.ReportTableID WHERE rtf.ReportTableID = @ReportTableID AND IsVisible = 1 AND IsReadOnly = @IsReadOnly ORDER BY rtf.Alias";
+        command.CommandText = "SELECT rtf.*, rt.Alias AS TableAlias FROM ReportTableFields rtf LEFT JOIN ReportTables rt ON rt.ReportTableID = rtf.ReportTableID WHERE rtf.ReportTableID = @ReportTableID AND (IsVisible = 1 OR @OrganizationID=1078) AND IsReadOnly = @IsReadOnly ORDER BY rtf.Alias";
         command.CommandType = CommandType.Text;
         command.Parameters.AddWithValue("ReportTableID", reportTableID);
         command.Parameters.AddWithValue("IsReadOnly", isReadOnly);
+        command.Parameters.AddWithValue("OrganizationID", LoginUser.OrganizationID);
         Fill(command);
       }
     }
@@ -40,9 +42,10 @@ namespace TeamSupport.Data
     {
       using (SqlCommand command = new SqlCommand())
       {
-        command.CommandText = "SELECT rtf.*, rt.Alias AS TableAlias FROM ReportTableFields rtf LEFT JOIN ReportTables rt ON rt.ReportTableID = rtf.ReportTableID WHERE rtf.ReportTableID = @ReportTableID AND IsVisible = 1 ORDER BY rtf.Alias";
+        command.CommandText = "SELECT rtf.*, rt.Alias AS TableAlias FROM ReportTableFields rtf LEFT JOIN ReportTables rt ON rt.ReportTableID = rtf.ReportTableID WHERE rtf.ReportTableID = @ReportTableID AND (IsVisible = 1 OR @OrganizationID=1078) ORDER BY rtf.Alias";
         command.CommandType = CommandType.Text;
         command.Parameters.AddWithValue("ReportTableID", reportTableID);
+        command.Parameters.AddWithValue("OrganizationID", LoginUser.OrganizationID);
         Fill(command);
       }
     }
@@ -58,9 +61,10 @@ ON (rs.ReportCategoryTableID = rtf.ReportTableID OR rs.ReportTableID = rtf.Repor
 LEFT JOIN ReportTables rt 
 ON rt.ReportTableID = rtf.@ReportSubcategoryID
 WHERE rs.ReportSubcategoryID = 6
-AND IsVisible = 1 ORDER BY rt.Alias, Alias";
+AND (IsVisible = 1 OR @OrganizationID=1078) ORDER BY rt.Alias, Alias";
         command.CommandType = CommandType.Text;
         command.Parameters.AddWithValue("ReportSubcategoryID", reportSubcategoryID);
+        command.Parameters.AddWithValue("OrganizationID", LoginUser.OrganizationID);
         Fill(command);
       }
     }
