b2d585b8262f9613bcbae0b6bee74e43ae046742
lperezpicasso <lperezpicasso@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2014-06-26 04:19:07 +0000

    Added logs to CrmPool
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@2095 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.CrmIntegration/CrmPool.vb
+++ TeamSupport.CrmIntegration/CrmPool.vb
@@ -46,25 +46,37 @@ Namespace TeamSupport
 
 
       Public Overrides Sub Run()
+        MyBase.Logs.WriteEvent("CrmPool is running...", true)
         Dim maxThreads As Integer = Settings.ReadInt("Max Worker Processes", 1)
+        MyBase.Logs.WriteEvent("maxThreads: " + maxThreads.ToString(), true)
 
-
+        MyBase.Logs.WriteEvent("IsStopped: " + IsStopped.ToString(), true)
+        MyBase.Logs.WriteEvent("Threads count: " + _threads.Count.ToString(), true)
         If _threads.Count > 0 Then
           For index = _threads.Count - 1 To 0 Step -1
+            MyBase.Logs.WriteEvent("Checking thread index: " + index.ToString(), true)
             If IsStopped Then
               _threads(index).Stop()
+              MyBase.Logs.WriteEvent("Thread index: " + index.ToString() + " stopped.", true)
               _threads.RemoveAt(index)
+              MyBase.Logs.WriteEvent("Thread index: " + index.ToString() + " removed.", true)
             ElseIf (_threads(index).IsStopped) Then
+              MyBase.Logs.WriteEvent("Thread index: " + index.ToString() + " is stopped.", true)
               _threads.RemoveAt(index)
+              MyBase.Logs.WriteEvent("Thread index: " + index.ToString() + " removed.", true)
             End If
           Next
         End If
 
-        If IsStopped Then Return
+        If IsStopped Then
+          MyBase.Logs.WriteEvent("CrmPool finishing as is stopped", true)
+          Return
+        End If
 
 
         'See if we have reached our max thread coutn
         If _threads.Count >= maxThreads Then
+          MyBase.Logs.WriteEvent("CrmPool finishing as its threads count exceeds maxThreads", true)
           Return
         End If
 
@@ -72,16 +84,25 @@ Namespace TeamSupport
         Dim links As New CRMLinkTable(_loginUser)
         links.LoadActive()
 
+        MyBase.Logs.WriteEvent("Active links count: " + links.Count().ToString(), true)
+        Dim isAlreadyProcessingValue As Boolean = False
         For Each link As CRMLinkTableItem In links
+          MyBase.Logs.WriteEvent("Checking link type: " + link.CRMType + " of OrgID: " + link.OrganizationID.ToString(), true)
           If _threads.Count >= maxThreads Then
+            MyBase.Logs.WriteEvent("CrmPool finishing as its threads count exceeds maxThreads", true)
             Return
           End If
 
-          If Not IsAlreadyProcessing(link.CRMLinkID) Then
+          isAlreadyProcessingValue = IsAlreadyProcessing(link.CRMLinkID)
+          MyBase.Logs.WriteEvent("isAlreadyProcessing: " + isAlreadyProcessingValue.ToString(), true)
+          If Not isAlreadyProcessingValue Then
 
             Dim crmProcessor As New CrmProcessor(link.CRMLinkID)
+            MyBase.Logs.WriteEvent("link CrmProcessor instantiated.", true)
             crmProcessor.Start()
+            MyBase.Logs.WriteEvent("link CrmProcessor started.", true)
             _threads.Add(crmProcessor)
+            MyBase.Logs.WriteEvent("link CrmProcessor thread added.", true)
           End If
         Next
       End Sub
