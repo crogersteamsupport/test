f65a3b8f96ae38c3e738385c7386be59d590f240
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2014-07-25 17:11:50 +0000

    Added marketing JS
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@2157 02885c34-e171-44ab-8435-42fb7a3c3aaf

---
+++ WebApp/Resources/Js/tsm.js
@@ -0,0 +1,6 @@
+﻿  <script type="text/javascript">
+    function getURLParameter(name) { return decodeURIComponent((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1]); }
+
+    $(document).ready(function () {
+    });
+  </script>
