a1d1596b6f6ff5b11c99d100cb001e98256ad3d5
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2014-10-30 20:13:32 +0000

    Import tweak
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@2425 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.Data/Classes/Importer.cs
+++ TeamSupport.Data/Classes/Importer.cs
@@ -1049,16 +1049,7 @@ namespace TeamSupport.Data
 
 
 
-        Users existingUsers = new Users(_loginUser);
-        existingUsers.LoadByName(row["FirstName"].ToString().Trim() + " " + row["LastName"].ToString().Trim(), organization.OrganizationID, false, false, false);
-        if (existingUsers.Count > 0)
-        {
-          existingUsers[0].ImportID = "[contact]" + row["ContactID"].ToString().Trim();
-          existingUsers.Save();
-          _log.AppendError(row, "Contact skipped due to already exists.");
-          continue;
-        }
-        /*
+       /*
         Users existingUsers = new Users(_loginUser);
         existingUsers.LoadByEmail(organization.OrganizationID, row["Email"].ToString().Trim());
 
