9affc96b00652c0eb752a415d8b857b5ea583cbb
gphillips <gphillips@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2014-11-22 06:37:10 +0000

    Signalr Revert
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@2543 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- SignalR/App_Code/Socket.cs
+++ SignalR/App_Code/Socket.cs
@@ -27,7 +27,7 @@ public class Socket : Hub
     {
     }
 
-    public override Task OnDisconnected(bool stopCalled)
+    public override Task OnDisconnected()
     {
       try
       {
@@ -48,7 +48,7 @@ public class Socket : Hub
         }
         Clients.All.serverleave(Context.ConnectionId, DateTime.Now.ToString());
 
-        return base.OnDisconnected(stopCalled);
+        return base.OnDisconnected();
       }
       catch (Exception ex)
       {
@@ -265,7 +265,7 @@ public class TicketSocket : Hub{
     {
     }
 
-    public override Task OnDisconnected(bool stopCalled)
+    public override Task OnDisconnected()
     {
         try
         {
@@ -280,7 +280,7 @@ public class TicketSocket : Hub{
             }
             Clients.All.serverleave(Context.ConnectionId, DateTime.Now.ToString());
 
-            return base.OnDisconnected(stopCalled);
+            return base.OnDisconnected();
         }
         catch (Exception ex)
         {
