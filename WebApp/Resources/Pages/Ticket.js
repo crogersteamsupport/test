4a2630cad1fbea51cfbd54d46afd217132caea09
MattT <MattT@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-04-02 17:56:52 +0000

    Adding back in TinyMCE update and table border fix.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@2968 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- WebApp/Resources/Pages/Ticket.js
+++ WebApp/Resources/Pages/Ticket.js
@@ -2543,7 +2543,7 @@ var initEditor = function (element, init) {
       toolbar2: "alignleft aligncenter alignright alignjustify | forecolor backcolor | fontselect fontsizeselect | bold italic underline strikethrough blockquote | code | table",
       statusbar : false,
       gecko_spellcheck: true,
-      extended_valid_elements: "a[accesskey|charset|class|coords|dir<ltr?rtl|href|hreflang|id|lang|name|onblur|onclick|ondblclick|onfocus|onkeydown|onkeypress|onkeyup|onmousedown|onmousemove|onmouseout|onmouseover|onmouseup|rel|rev|shape<circle?default?poly?rect|style|tabindex|title|target|type],script[charset|defer|language|src|type],table[class=table|border:1]",
+      extended_valid_elements: "a[accesskey|charset|class|coords|dir<ltr?rtl|href|hreflang|id|lang|name|onblur|onclick|ondblclick|onfocus|onkeydown|onkeypress|onkeyup|onmousedown|onmousemove|onmouseout|onmouseover|onmouseup|rel|rev|shape<circle?default?poly?rect|style|tabindex|title|target|type],script[charset|defer|language|src|type]",
       content_css: "../Css/jquery-ui-latest.custom.css,../Css/editor.css",
       body_class: "ui-widget ui-widget-content",
 
