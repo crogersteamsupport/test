f1f9f90428a1123e32f25be0be88ce82b7c06599
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-05-24 06:03:24 +0000

    Host File Edit
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@3243 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- Add TSDev Hosts.cmd
+++ Add TSDev Hosts.cmd
@@ -1,6 +1,11 @@
 FIND /C /I "app.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
 IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				trunk.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
-
 FIND /C /I "portal.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
 IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				signalr.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
+FIND /C /I "portal.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				portal.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
+FIND /C /I "portal.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				mobile.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
+FIND /C /I "portal.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				m.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
 
