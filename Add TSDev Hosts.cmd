1fba6fb5526d835f29aeb9c978de12b8035841a1
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-05-24 05:34:21 +0000

    Host File Edit
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@3242 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- Add TSDev Hosts.cmd
+++ Add TSDev Hosts.cmd
@@ -1,14 +1,6 @@
 FIND /C /I "app.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				app.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				trunk.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
 
 FIND /C /I "portal.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				portal.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				signalr.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
 
-FIND /C /I "beta.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				beta.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
-
-FIND /C /I "alpha.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				alpha.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
-
-FIND /C /I "updates.tsdev.com" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO ^127.0.0.1				updates.tsdev.com>>%WINDIR%\system32\drivers\etc\hosts
