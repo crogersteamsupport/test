049d4f8774f35e500819769f6f361fd3cd21da45
lperezpicasso <lperezpicasso@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-06-19 19:04:50 +0000

    [Tickets:19027] Adjust Safari on Mac screen recorder images.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@3381 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- WebApp/Resources/Pages/Ticket.js
+++ WebApp/Resources/Pages/Ticket.js
@@ -2861,11 +2861,11 @@ for more information or use an alternate browser like Firefox or Internet Explor
                       top.Ts.Services.Settings.ReadUserSetting('ReadScreenRecordingSafariInfo', 0, function (alreadyReadInfo) {
                         if (alreadyReadInfo == 0) {
                           $(".pAllowPluginsToRunInstructions").html("\
-The following steps will refresh your browser<br> \
+The following steps will refresh your browser<br><br> \
 1. Allow the screen recorder Java plugins to run on your browser by clicking on the Trust button at the top of the page: \
-<img src='../Images/icons/SafariInMacPluginDialog.png' alt='plugin dialog' width='100%' style='margin-top: 10px'><br> \
+<img src='../Images/icons/SafariInMacPluginDialog.png' alt='plugin dialog' width='50%' style='margin-top: 10px'><br><br> \
 2. Navigate to Safari > Preferences > Security > Internet Plugins - Website Settings > Java and change the app.teamsupport.com setting to Run in Unsafe Mode and click on the Trust button: \
-<img src='../Images/icons/SafariInMacUnsafeModeDialog.png' alt='plugin dialog' width='100%' style='margin-top: 10px'>");
+<img src='../Images/icons/SafariInMacUnsafeModeDialog.png' alt='plugin dialog' width='50%' style='margin-top: 10px'>");
                           $('.divScreenRecorderMessages').show();
                         }
                       });
