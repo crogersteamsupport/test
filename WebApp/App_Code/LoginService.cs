0c87263555acc10799d5c3a5996ff8e1b706a729
vvillagomez <vvillagomez@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-09-01 17:48:39 +0000

    LoginService service committed. Empty shell for now.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@3639 02885c34-e171-44ab-8435-42fb7a3c3aaf

---
+++ WebApp/App_Code/LoginService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+
+namespace TSWebServices
+{
+	[ScriptService]
+	[WebService(Namespace = "http://teamsupport.com/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	public class LoginService : System.Web.Services.WebService
+	{
+		public LoginService()
+		{
+
+			//Uncomment the following line if using designed components 
+			//InitializeComponent(); 
+		}
+
+		[WebMethod]
+		public int SignIn(string email, string password, int? organizationId, bool verificationRequired)
+		{
+			int result = 0;
+
+			return result;
+		}
+
+	}
+}
