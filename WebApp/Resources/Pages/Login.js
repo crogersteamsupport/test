99de8da85dfbf50f9432110efcc1ab6cfc186881
MattT <MattT@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-09-02 20:00:46 +0000

    Add in redirect after successful login as well as company lookup based on email.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@3652 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- WebApp/Resources/Pages/Login.js
+++ WebApp/Resources/Pages/Login.js
@@ -4,28 +4,33 @@
 //VerificationNeeded = 3
 
 var loginService = '/Services/LoginService.asmx/';
+var returnURL = '/';
 
 $(document).ready(function () {
+  returnURL = top.Ts.Utils.getQueryValue("ReturnUrl", window);
+
   $('#signIn').click(function (e) {
     //attempt to sign the user in and process the return request
     e.preventDefault();
     var email = $('#inputEmail').val();
     var org = $('#orgSelect').val();
     if (org == "") org = null;
-    var signInData = { email: email, password: $('#inputPassword').val(), organizationId: 1078, verificationRequired: false };
+    var signInData = { email: email, password: $('#inputPassword').val(), organizationId: org, verificationRequired: false };
 
     IssueAjaxRequest(loginService, "SignIn", signInData,
     function (result) {
       //success
       debugger
-      alert(result);
+      if (result.Result == 1) {
+        window.location = returnURL;
+      }
     },
     function (error) {
       alert('There was a error signing you in.  Please try again. ')
     });
   });
 
-  $('#inputEmail').keyup(function (e) {
+  $('#inputEmail').change(function (e) {
     CheckEmailForOrgs($(this).val())
   });
 
@@ -39,19 +44,44 @@ $(document).ready(function () {
     //redirect to signup form
   });
 
-  function CheckEmailForOrgs(email) {
-    //Code to lookup the email address entered and check for a valid user with multiple orgs. 
-    var emailLookupData = { email: email }
-    IssueAjaxRequest(loginService, "GetCompanies", emailLookupData,
-      function (result) {
-        //success
-        debugger
-      },
-      function (error) {
+function CheckEmailForOrgs(email) { 
+  //Code to lookup the email address entered and check for a valid user with multiple orgs. 
+  var emailLookupData = { email: email }
+  IssueAjaxRequest(loginService, "GetCompanies", emailLookupData,
+    function (result) {
+      //success
+      LoadCompanies(result);
+    },
+    function (error) {
         
-      });
+    });
+  }
+});
+
+function LoadCompanies(companies) {
+  var companySelect = $('#orgSelect');
+  companySelect.empty();
+
+  if (companies && companies.length > 0) {
+    for (var i = 0; i < companies.length; i++) {
+      $('<option>').attr('value', companies[i].ID).text(companies[i].Label).appendTo(companySelect);
     }
-  });
+    companySelect.show();
+  }
+  else companySelect.hide();
+}
+
+//function getQueryValue(name, wnd) {
+//  if (!wnd) wnd = window;
+//  params = wnd.location.search.substring(1);
+//  name = name.toLowerCase();
+//  param = params.split("&");
+//  for (i = 0; i < param.length; i++) {
+//    value = param[i].split("=");
+//    if (value[0].toLowerCase() == name) { return unescape(value[1]); }
+//  }
+//  return null;
+//};
 
 function IssueAjaxRequest(service, method, data, successCallback, errorCallback) {
   $.ajax({
