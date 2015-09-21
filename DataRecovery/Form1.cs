0fe8ee8edb96fbac4be796b0c04998b2102f794f
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-09-21 13:36:47 +0000

    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@3817 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- DataRecovery/Form1.cs
+++ DataRecovery/Form1.cs
@@ -79,7 +79,7 @@ namespace DataRecovery
 
 		private void SaveOrg(int orgID, string result)
 		{
-			SqlExecutor.ExecuteNonQuery(GetCorrupteLoginUser(), "UPDATE OrgMoveEvent SET Result = '"+result+"', HasExecuted = 1");
+			SqlExecutor.ExecuteNonQuery(GetCorrupteLoginUser(), "UPDATE OrgMoveEvent SET Result = '"+result+"', HasExecuted = 1 WHERE OrgID = " + orgID.ToString());
 		}
 
 		private void RecoverProducts(int orgID)
