680fa16c3eb15a1180a7f61d475fe518b2e28350
MattT <MattT@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-10-22 16:05:31 +0000

    add in key value and new login process
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@3916 02885c34-e171-44ab-8435-42fb7a3c3aaf

---
+++ WebApp/Login2.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" %>
+
+<!DOCTYPE html>
+
+<script runat="server">
+
+</script>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>TeamSupport - Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="vcr/1_9_0/Css/bootstrap3.min.css" rel="stylesheet" type="text/css" />
+  <link href="vcr/1_9_0/Pages/Login.css" rel="stylesheet" type="text/css" />
+  <script src="vcr/1_9_0/Js/jquery-latest.min.js" type="text/javascript"></script>
+  <script src="vcr/1_9_0/Js/bootstrap3.min.js" type="text/javascript"></script>
+  <script src="vcr/1_9_0/Pages/Login2.js" type="text/javascript"></script>
+  <script src="vcr/1_9_0/Js/Ts/ts.utils.js"></script>
+</head>
+<body>
+  <header>
+    <div class="headerDiv">
+      <img alt="Logo" src="vcr/1_9_0/images/page_header.png" />
+    </div>
+  </header>
+  <div class="container-fluid">
+    <div class="card card-container">
+      <h3 class="pageTitle">Sign in to TeamSupport</h3>
+	 	<h6 class="text-muted" style="text-align: center; margin-bottom: 0px; display:none;">
+	 		Login attempt <span id="numbAttempts">2</span> of 10
+	 	</h6>
+      <form class="form-signin">
+        <span id="reauth-email" class="reauth-email"></span>
+        <%--<input type="email" id="inputEmail" class="form-control" placeholder="Email address" autocomplete="off" required autofocus>--%>
+        <label id="email">email address</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" autocomplete="off" required autofocus>
+      <select id="orgSelect" class="form-control" style="display:none;">
+      </select>
+        <div id="remember" class="checkbox">
+          <label>
+            <input id="rememberMe" type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button id="signIn" class="btn btn-lg btn-primary btn-block btn-signin">Sign in</button>
+      </form>
+      <a id="forgotPW" href="ResetPassword.aspx?reason=forgot" class="forgot-password">
+        Forgot your password?
+      </a>
+      <div id="loginError" class="alert alert-danger" style="display:none; margin-top:15px; margin-bottom: 0px;" role="alert"></div>
+      <footer class="container">
+        <div class="row">
+          <div class="col-xs-6" style="border-right: 1px solid rgba(117, 117, 117, 0.15);">
+			 	<a href="http://www.teamsupport.com/customer-support-software-free-trial">Create a free account</a>
+          </div>
+          <div class="col-xs-6">
+            <a href="http://m.teamsupport.com">Mobile Site</a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+ </body>
+</html>
---
+++ WebApp/Resources/Pages/Login2.js
@@ -0,0 +1,90 @@
+﻿var loginService = '/Services/LoginService.asmx/';
+var resourcesURL = '/vcr/1_9_0/Pages/';
+var returnURL = '/default.aspx';
+var orgCookie = null;
+var emailAddress = null;
+var orgId = null;
+
+$(document).ready(function () {
+	returnURL = top.Ts.Utils.getQueryValue("ReturnUrl", window);
+	emailAddress = top.Ts.Utils.getQueryValue("Email", window);
+	orgId = top.Ts.Utils.getQueryValue("OrgID", window);
+	$('#email').text(emailAddress);
+	supportToken = top.Ts.Utils.getQueryValue("SupportToken", window);
+	if (supportToken && supportToken != '')
+	{
+		IssueAjaxRequest(loginService, "SupportSignIn", { token: supportToken }, function (result) { window.location = '/Default.aspx'; }, function () { });
+		return;
+	}
+
+  $('#signIn').click(function (e) {
+    e.preventDefault();
+    var rememberMe = $('#rememberMe').is(":checked"); 
+    if (orgId == null) orgId = 1;
+    var signInData = { email: emailAddress, password: $('#inputPassword').val(), organizationId: orgId, verificationRequired: true, rememberMe: rememberMe };
+
+    IssueAjaxRequest(loginService, "SignIn", signInData,
+    function (result) {
+      switch (result.Result) {//Unknown = 0, Success = 1, Fail = 2, VerificationNeeded = 3, VerificationSetupNeeded = 4, ExipredPassword = 5
+        case 1:
+        	window.location = returnURL;
+          break;
+        case 3:
+          window.location = '/LoginTwoStep.aspx?UserID=' + result.UserId;
+          break;
+        case 4:
+          window.location = resourcesURL + 'LoginTwoStepSetup.html?UserID=' + result.UserId;
+          break;
+			case 5:
+				window.location = resourcesURL + result.RedirectURL;
+      		break;
+        default:
+        	$('#loginError').text(result.Error).show();
+        	if (result.LoginFailedAttempts > 0 && result.LoginFailedAttempts <= 10) {
+        		$('#numbAttempts').text(result.LoginFailedAttempts).parent().show();
+        	}
+        	else $('#numbAttempts').parent().hide();
+          break;
+      }
+    },
+    function (error) {
+      alert('There was a error signing you in.  Please try again. ')
+    });
+  });
+
+  getRememberMe();
+});
+
+function getRememberMe() {
+    var cookies = Ts.Utils.getCookie('rm');
+    if (cookies != null) {
+        $('#inputEmail').val(cookies["a"]).change();
+        orgCookie = cookies["b"];
+        $('#rememberMe').attr('checked', 'checked');
+    }
+}
+
+function IssueAjaxRequest(service, method, data, successCallback, errorCallback) {
+  $.ajax({
+    type: "POST",
+    url: service + method,
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+	 cache: false,
+    dataFilter: function (data) {
+      var jsonResult = eval('(' + data + ')');
+      if (jsonResult.hasOwnProperty('d'))
+        return jsonResult.d;
+      else
+        return jsonResult;
+    },
+    success: function (jsonResult) {
+      successCallback(jsonResult);
+      //alert(jsonResult.UserId + ' ' + jsonResult.OrganizationId + ' ' + jsonResult.Error + ' ' + jsonResult.Result + ' ' + jsonResult.ResultValue);
+    },
+    error: function (error, errorStatus, errorThrown) {
+      if (errorCallback) errorCallback(error);
+    }
+  });
+}
