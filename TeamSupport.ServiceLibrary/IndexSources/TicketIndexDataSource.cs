c43807928ab2b9721146423db2764f0bd21ba44f
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2015-12-16 18:03:23 +0000

    Removed the temp ecsi index testing stuff
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@4055 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.ServiceLibrary/IndexSources/TicketIndexDataSource.cs
+++ TeamSupport.ServiceLibrary/IndexSources/TicketIndexDataSource.cs
@@ -7,31 +7,30 @@ using TeamSupport.Data;
 
 namespace TeamSupport.ServiceLibrary
 {
-    [Serializable]
-    class TicketIndexDataSource : IndexDataSource
+  [Serializable]
+  class TicketIndexDataSource : IndexDataSource
+  {
+    protected TicketIndexDataSource() { }
+
+    public TicketIndexDataSource(LoginUser loginUser, int maxCount, int organizationID, string table, bool isRebuilding, Logs logs)
+      : base(loginUser, maxCount, organizationID, table, isRebuilding, logs)
     {
-        protected TicketIndexDataSource() { }
-
-        public TicketIndexDataSource(LoginUser loginUser, int maxCount, int organizationID, string table, bool isRebuilding, Logs logs)
-          : base(loginUser, maxCount, organizationID, table, isRebuilding, logs)
-        {
-        }
+    }
 
 
-        override protected void GetNextRecord()
-        {
-            TicketsViewItem ticket = TicketsView.GetTicketsViewItem(_loginUser, _itemIDList[_rowIndex]);
-            _lastItemID = ticket.TicketID;
-            UpdatedItems.Add((int)_lastItemID);
+    override protected void GetNextRecord()
+    {
+      TicketsViewItem ticket = TicketsView.GetTicketsViewItem(_loginUser, _itemIDList[_rowIndex]);
+      _lastItemID = ticket.TicketID;
+      UpdatedItems.Add((int)_lastItemID);
 
-            StringBuilder actionsBuilder = new StringBuilder();
-            Actions actions = new Actions(_loginUser);
-            actions.LoadByTicketID(ticket.TicketID);
-            foreach (TeamSupport.Data.Action action in actions)
-            {
-                string actionText = action.Description;
+      StringBuilder actionsBuilder = new StringBuilder();
+      Actions actions = new Actions(_loginUser);
+      actions.LoadByTicketID(ticket.TicketID);
+      foreach (TeamSupport.Data.Action action in actions)
+      {
+        string actionText = action.Description;
 
-                /*
         if (!action.IsClean)
         {
           try
@@ -48,58 +47,57 @@ namespace TeamSupport.ServiceLibrary
             _logs.WriteEvent("Unable to sanitize action: " + action.ActionID);
           }
         }
-        */
 
-                try
-                {
-                    //actionText = HtmlToText.ConvertHtml(actionText);
-                    actionsBuilder.AppendLine(actionText);
-                }
-                catch (Exception ex)
-                {
-                    _logs.WriteEvent("Unable to convert action html: " + action.ActionID);
-                    _logs.WriteException(ex);
-                }
-            }
-
-            DocText = actionsBuilder.ToString();
-
-            _docFields.Clear();
-            foreach (DataColumn column in ticket.Collection.Table.Columns)
-            {
-                object value = ticket.Row[column];
-                string s = value == null || value == DBNull.Value ? "" : value.ToString();
-                AddDocField(column.ColumnName, s);
-            }
-
-            CustomValues customValues = new CustomValues(_loginUser);
-            customValues.LoadByReferenceType(_organizationID, ReferenceType.Tickets, ticket.TicketTypeID, ticket.TicketID);
-
-            foreach (CustomValue value in customValues)
-            {
-                object o = value.Row["CustomValue"];
-                string s = o == null || o == DBNull.Value ? "" : o.ToString();
-                AddDocField(value.Row["Name"].ToString(), s);
-            }
-            DocFields = _docFields.ToString();
-
-            DocIsFile = false;
-            DocName = ticket.TicketID.ToString();
-            DocDisplayName = string.Format("{0}: {1}", ticket.TicketNumber.ToString(), ticket.Name);
-            DocCreatedDate = (DateTime)ticket.Row["DateCreated"];
-            DocModifiedDate = (DateTime)ticket.Row["DateModified"];
-        }
-
-
-
-        override protected void LoadData()
+        try
         {
-            TicketsView tickets = new TicketsView(_loginUser);
-            tickets.LoadForIndexing(_organizationID, _maxCount, _isRebuilding);
-            foreach (TicketsViewItem ticket in tickets)
-            {
-                _itemIDList.Add(ticket.TicketID);
-            }
+          //actionText = HtmlToText.ConvertHtml(actionText);
+          actionsBuilder.AppendLine(actionText);
         }
+        catch (Exception ex)
+        {
+          _logs.WriteEvent("Unable to convert action html: " + action.ActionID);
+          _logs.WriteException(ex);
+        }
+      }
+
+      DocText = actionsBuilder.ToString();
+
+      _docFields.Clear();
+      foreach (DataColumn column in ticket.Collection.Table.Columns)
+	    {
+        object value = ticket.Row[column];
+        string s = value == null || value == DBNull.Value ? "" : value.ToString();
+        AddDocField(column.ColumnName, s);
+      }
+
+      CustomValues customValues = new CustomValues(_loginUser);
+      customValues.LoadByReferenceType(_organizationID, ReferenceType.Tickets, ticket.TicketTypeID, ticket.TicketID);
+
+      foreach (CustomValue value in customValues)
+      {
+        object o = value.Row["CustomValue"];
+        string s = o == null || o == DBNull.Value ? "" : o.ToString();
+        AddDocField(value.Row["Name"].ToString(), s);
+      }
+      DocFields = _docFields.ToString();
+
+      DocIsFile       = false;
+      DocName         = ticket.TicketID.ToString();
+      DocDisplayName  = string.Format("{0}: {1}", ticket.TicketNumber.ToString(), ticket.Name);
+      DocCreatedDate  = (DateTime)ticket.Row["DateCreated"];
+      DocModifiedDate = (DateTime)ticket.Row["DateModified"];
     }
+
+
+
+    override protected void LoadData()
+    {
+      TicketsView tickets = new TicketsView(_loginUser);
+      tickets.LoadForIndexing(_organizationID, _maxCount, _isRebuilding);
+      foreach (TicketsViewItem ticket in tickets)
+      {
+        _itemIDList.Add(ticket.TicketID);
+      }
+    }
+  }
 }
