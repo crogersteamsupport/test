7cf20481b733723aee58c6203450f05ed159121c
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2016-01-11 21:03:48 +0000

    Added logging to API
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@4099 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.Api/RestCommandHandler.cs
+++ TeamSupport.Api/RestCommandHandler.cs
@@ -12,96 +12,100 @@ using System.Web.SessionState;
 
 namespace TeamSupport.Api
 {
-  public class RestCommandHandler: IHttpHandler, IRequiresSessionState
-  {
-    private LoginUser _loginUser;
-    private Organization _organization;
-
-    #region IHttpHandler Members
-
-    public bool IsReusable
+    public class RestCommandHandler : IHttpHandler, IRequiresSessionState
     {
-      get { return false; }
-    }
+        private LoginUser _loginUser;
+        private Organization _organization;
 
-    public void ProcessRequest(HttpContext context)
-    {
-      try
-      {
-			string s = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["MainConnection"].ConnectionString;
-			var start = s.IndexOf('=');
-			var end = s.IndexOf(';');
-			var subStr = s.Substring(start + 1, end - (start + 1));
+        #region IHttpHandler Members
 
-			string connection = s.Replace(subStr, "API");
-
-        int organizationID = int.Parse(context.Request.Headers["OrganizationID"]);
-        _loginUser = new LoginUser(connection, (int)SystemUser.API, organizationID, null);
-        _organization = Organizations.GetOrganization(_loginUser, organizationID);
-      }
-      catch (Exception)
-      {
-        throw new RestException(HttpStatusCode.Unauthorized);
-        
-      }
-
-      ApiLog log = new ApiLogs(_loginUser).AddNewApiLog();
-
-
-      context.Response.StatusCode = (int)HttpStatusCode.OK;
-      context.Response.ContentType = "text/plain";
-      try
-      {
-        try
+        public bool IsReusable
         {
-          RestCommand command = new RestCommand(_loginUser, _organization, context);
-          log.OrganizationID = _organization.OrganizationID;
-          log.RequestBody = command.Data;
-          log.Verb = context.Request.HttpMethod.ToUpper();
-          log.Url = context.Request.Url.OriginalString;
-          log.IPAddress = context.Request.UserHostAddress;
-          RestProcessor processor = new RestProcessor(command);
-
-          int companyId = command.IsCustomerOnly ? (int)command.Organization.ParentID : _loginUser.OrganizationID;
-          int apiRequestLimit = command.IsCustomerOnly ? Organizations.GetOrganization(_loginUser, companyId).APIRequestLimit : _organization.APIRequestLimit;
-          int apiRequestCount = ApiLogs.GetDailyRequestCount(_loginUser, companyId);
-
-          if (apiRequestCount > apiRequestLimit)
-            throw new RestException(HttpStatusCode.Forbidden, "You have exceeded your 24 hour API request limit of " + _organization.APIRequestLimit.ToString() + ".");
-
-          processor.Process();
-
-        }
-        catch (Exception ex)
-        {
-          ExceptionLogs.LogException(_loginUser, ex, "API", string.Format("OrgID: {0}{1}Verb: {2}{1}Url: {3}{1}Body: {4}", log.OrganizationID, Environment.NewLine, log.Verb, log.Url, log.RequestBody));
-          if (!(ex is RestException))
-          {
-            throw new RestException(HttpStatusCode.InternalServerError, "Internal Server Error: " + ex.Message + ex.StackTrace, ex);
-          }
-          else
-          {
-            throw ex;
-          }
+            get { return false; }
         }
 
-      }
-      catch (RestException rex)
-      {
-        context.Response.ContentType = "text/plain";
-        context.Response.StatusCode = (int)rex.HttpStatusCode;
-        log.StatusCode = context.Response.StatusCode;
-        log.Collection.Save();
-        context.Response.ClearContent();
-        context.Response.Write(rex.Message);
-        context.Response.End();
-      }
-      log.StatusCode = context.Response.StatusCode;
-      log.Collection.Save();
-      context.Response.End();
+        public void ProcessRequest(HttpContext context)
+        {
+            int organizationID = int.Parse(context.Request.Headers["OrganizationID"]);
 
 
+            try
+
+            {
+                string s = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["MainConnection"].ConnectionString;
+                var start = s.IndexOf('=');
+                var end = s.IndexOf(';');
+                var subStr = s.Substring(start + 1, end - (start + 1));
+
+                string connection = s.Replace(subStr, "API");
+
+                _loginUser = new LoginUser(connection, (int)SystemUser.API, organizationID, null);
+                _organization = Organizations.GetOrganization(_loginUser, organizationID);
+            }
+            catch (Exception ex)
+            {
+                ExceptionLogs.LogException(_loginUser, ex, "OrgID: " + organizationID.ToString());
+              throw new RestException(HttpStatusCode.Unauthorized);
+
+            }
+
+            ApiLog log = new ApiLogs(_loginUser).AddNewApiLog();
+
+
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+            context.Response.ContentType = "text/plain";
+            try
+            {
+                try
+                {
+                    RestCommand command = new RestCommand(_loginUser, _organization, context);
+                    log.OrganizationID = _organization.OrganizationID;
+                    log.RequestBody = command.Data;
+                    log.Verb = context.Request.HttpMethod.ToUpper();
+                    log.Url = context.Request.Url.OriginalString;
+                    log.IPAddress = context.Request.UserHostAddress;
+                    RestProcessor processor = new RestProcessor(command);
+
+                    int companyId = command.IsCustomerOnly ? (int)command.Organization.ParentID : _loginUser.OrganizationID;
+                    int apiRequestLimit = command.IsCustomerOnly ? Organizations.GetOrganization(_loginUser, companyId).APIRequestLimit : _organization.APIRequestLimit;
+                    int apiRequestCount = ApiLogs.GetDailyRequestCount(_loginUser, companyId);
+
+                    if (apiRequestCount > apiRequestLimit)
+                        throw new RestException(HttpStatusCode.Forbidden, "You have exceeded your 24 hour API request limit of " + _organization.APIRequestLimit.ToString() + ".");
+
+                    processor.Process();
+
+                }
+                catch (Exception ex)
+                {
+                    ExceptionLogs.LogException(_loginUser, ex, "API", string.Format("OrgID: {0}{1}Verb: {2}{1}Url: {3}{1}Body: {4}", log.OrganizationID, Environment.NewLine, log.Verb, log.Url, log.RequestBody));
+                    if (!(ex is RestException))
+                    {
+                        throw new RestException(HttpStatusCode.InternalServerError, "Internal Server Error: " + ex.Message + ex.StackTrace, ex);
+                    }
+                    else
+                    {
+                        throw ex;
+                    }
+                }
+
+            }
+            catch (RestException rex)
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.StatusCode = (int)rex.HttpStatusCode;
+                log.StatusCode = context.Response.StatusCode;
+                log.Collection.Save();
+                context.Response.ClearContent();
+                context.Response.Write(rex.Message);
+                context.Response.End();
+            }
+            log.StatusCode = context.Response.StatusCode;
+            log.Collection.Save();
+            context.Response.End();
+
+
+        }
+        #endregion
     }
-    #endregion
-  }
 }
