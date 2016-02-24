56752bfca08f6930ec804a79fc11887df845cc9a
gphillips <gphillips@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2016-02-24 18:10:06 +0000

    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@4251 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- WebApp/Resources/Js/Ts/ts.editor.js
+++ WebApp/Resources/Js/Ts/ts.editor.js
@@ -211,7 +211,7 @@
                 			OT.checkScreenSharingCapability(function (response) {
                 				if (!response.supported || response.extensionRegistered === false) {
                 					alert("This browser does not support screen sharing");
-                				} else if (response.extensionInstalled === false || response.extensionInstalled == null) {
+                				} else if (response.extensionInstalled === false) {
                 					// prompt to install the response.extensionRequired extension
                 					$('#ChromeInstallModal').modal('show');
                 				} else {
