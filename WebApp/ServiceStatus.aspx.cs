7bf345b31b683ba9e9e8032a0b931d594c736322
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2016-04-08 16:58:35 +0000

    Added some service checks for Panopta.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@4457 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- WebApp/ServiceStatus.aspx.cs
+++ WebApp/ServiceStatus.aspx.cs
@@ -11,59 +11,94 @@ public partial class ServiceStatus : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-      string name = "All services are ";
-      
-      bool flag = false;
-      if (Request["ServiceName"] != null)
-      {
-        Service service = Services.GetService(LoginUser.Anonymous, Request["ServiceName"], false);
-        if (service == null)
-        {
-          Response.Write("Service not found.");
-          Response.End();
-          return;
-        }
+        string name = "All services are ";
+        StringBuilder result = new StringBuilder();
 
-        if (!service.Enabled)
+        bool flag = false;
+        if (Request["ServiceName"] != null)
         {
-          Response.Write("Service is disabled.");
-          Response.End();
-          return;
-        }
+            Service service = Services.GetService(LoginUser.Anonymous, Request["ServiceName"], false);
+            if (service == null)
+            {
+                Response.Write("Service not found.");
+                Response.End();
+                return;
+            }
 
-        name = service.Name + " service is ";
-        if (service.LastStartTime != null) 
+            if (!service.Enabled)
+            {
+                Response.Write("Service is disabled.");
+                Response.End();
+                return;
+            }
+
+            name = service.Name + " service is ";
+            if (service.LastStartTime != null)
+            {
+                if (DateTime.Now.Subtract((DateTime)service.Row["HealthTime"]).TotalMinutes < service.HealthMaxMinutes)
+                {
+                    flag = true;
+                }
+            }
+        }
+        else
         {
-          if (DateTime.Now.Subtract((DateTime)service.Row["HealthTime"]).TotalMinutes < service.HealthMaxMinutes)
-          {
+            Services services = new Services(LoginUser.Anonymous);
+            services.LoadAll();
+            result.Append("<table>");
             flag = true;
-          }
+            foreach (Service service in services)
+            {
+                if (service.LastStartTime == null || !service.Enabled) continue;
+                if (DateTime.Now.Subtract((DateTime)service.Row["HealthTime"]).TotalMinutes > service.HealthMaxMinutes)
+                {
+                    flag = false;
+                    AddRow(result, service.Row["Name"].ToString(), "Past Health Time");
+                }
+            }
+
+            result.Append("</table>");
         }
-      }
-      else
-      {
-        Services services = new Services(LoginUser.Anonymous);
-        services.LoadAll();
-        flag = true;
-        foreach (Service service in services)
+
+
+        flag = CheckCount(flag, result, 0, "Outbound Email Delay", "There are outgoing email delays of more than 10 minutes, check EmailSender service", @"
+SELECT COUNT(*)
+FROM Emails e 
+WHERE IsWaiting = 1 
+AND e.Attempts < 1
+AND DATEDIFF(MINUTE, e.DateCreated, GETUTCDATE()) > 10
+");
+
+        flag = CheckCount(flag, result, 500, "Outbound Email Back Up", "There are too many out going emails pending, check EmailSender service", @"
+SELECT COUNT(*)
+FROM Emails e 
+WHERE IsWaiting = 1 
+");
+
+        flag = CheckCount(flag, result, 1000, "Email Processing Backed Up", "There are too many records in the EmailPosts table, check EmailProcessor service.", @"
+SELECT COUNT(*) FROM EmailPosts 
+");
+
+        result.Append(string.Format("<h2 style=\"color:{1};\">{0} is{2} running.</h2>", name, flag ? "green" : "red", flag ? "" : " NOT"));
+        litStatus.Text = result.ToString();
+
+    }
+
+    private static void AddRow(StringBuilder builder, string name, string message)
+    {
+        builder.Append(string.Format("<tr><td>{0}: </td><td>{1}</td></tr>", name, message));
+    }
+
+    private static bool CheckCount(bool flag, StringBuilder builder, int maxCount, string name, string failedMessage, string query)
+    {
+        int result = (int)SqlExecutor.ExecuteScalar(LoginUser.Anonymous, query);
+
+        if (result > maxCount)
         {
-          if (service.LastStartTime == null || !service.Enabled) continue;
-          if (DateTime.Now.Subtract((DateTime)service.Row["HealthTime"]).TotalMinutes > service.HealthMaxMinutes)
-          {
-            flag = false;
-            break;
-          }
+            AddRow(builder, name, failedMessage);
+            return false;
         }
-      }
 
-      if (flag)
-      {
-        litStatus.Text = "<h2 style=\"color:green;\">"+name+"running.</h2>";
-      }
-      else
-      {
-        litStatus.Text = "<h2 style=\"color:red;\">"+name+"not running.</h2>";
-      }
-
+        return flag;
     }
 }
