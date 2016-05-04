fe6aa9f842ea960e2e64cbb4ed5ac3db8bcd183e
MattT <MattT@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2016-05-04 17:26:33 +0000

    Push admin pages.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@4555 02885c34-e171-44ab-8435-42fb7a3c3aaf

---
+++ WebApp/Resources/Pages/Admin_Hub.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+	var encrypted = CryptoJS.AES.encrypt(Date.now() + "," + top.Ts.System.User.Email + "", top.Ts.System.Organization.PortalGuid);
+
+	top.Ts.Services.Admin.GetHubURL(function (url) {
+		$('#hub_admin').attr('src', "https://" + url + "/#/sso/" + encrypted)
+	});
+});
+
