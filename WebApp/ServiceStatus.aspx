a447256dff9bd97125fa1302496630307d4d8eb4
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2016-05-09 18:47:53 +0000

    Tweaked service status page
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@4566 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- WebApp/ServiceStatus.aspx
+++ WebApp/ServiceStatus.aspx
@@ -4,15 +4,65 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="vcr/1_9_0/Css/bootstrap3.min.css" rel="stylesheet" />
+
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-      <asp:Literal ID="litStatus" runat=server></asp:Literal>
+        <div class="container">
+            <div class="page-header">
+                <asp:Literal ID="litStatus" runat="server"></asp:Literal>
+            </div>
+
+            <div class="page-header">
+                <h2>Service Statuses</h2>
+            </div>
+
+            <div>
+                <table class="table table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>Service</th>
+                            <th>Status</th>
+                            <th>Last Time Checked</th>
+                            <th>Max Alert Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <asp:Literal ID="litStatusRows" runat="server"></asp:Literal>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="page-header">
+                <h2>Table Count Statuses</h2>
+            </div>
+
+            <div>
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Counted Item</th>
+                            <th>Status</th>
+                            <th>Max Rows</th>
+                            <th>Row Count</th>
+                            <th>Message</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <asp:Literal ID="litQueryRows" runat="server"></asp:Literal>
+                    </tbody>
+                </table>
+            </div>
 
 
-    </div>
+
+        </div>
     </form>
+    <script src="vcr/1_9_0/Js/bootstrap3.min.js"></script>
 </body>
 </html>
--- WebApp/ServiceStatus.aspx.cs
+++ WebApp/ServiceStatus.aspx.cs
@@ -12,58 +12,30 @@ public partial class ServiceStatus : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         string name = "All services are ";
-        StringBuilder result = new StringBuilder();
+        StringBuilder rowBuilder = new StringBuilder();
 
         bool isFailed = false;
-        if (Request["ServiceName"] != null)
+        Services services = new Services(LoginUser.Anonymous);
+        services.LoadAll();
+        isFailed = false;
+        foreach (Service service in services)
         {
-            Service service = Services.GetService(LoginUser.Anonymous, Request["ServiceName"], false);
-            if (service == null)
+            bool isGood = true;
+            if (service.LastStartTime == null || !service.Enabled) continue;
+            if (DateTime.Now.Subtract((DateTime)service.Row["HealthTime"]).TotalMinutes > service.HealthMaxMinutes)
             {
-                Response.Write("Service not found.");
-                Response.End();
-                return;
+                isFailed = true;
+                isGood = false;
             }
-
-            if (!service.Enabled)
-            {
-                Response.Write("Service is disabled.");
-                Response.End();
-                return;
-            }
-
-            name = service.Name + " service is ";
-            if (service.LastStartTime != null)
-            {
-                if (DateTime.Now.Subtract((DateTime)service.Row["HealthTime"]).TotalMinutes > service.HealthMaxMinutes)
-                {
-                    isFailed = true;
-                }
-            }
-        }
-        else
-        {
-            Services services = new Services(LoginUser.Anonymous);
-            services.LoadAll();
-            result.Append("<table>");
-            isFailed = false;
-            foreach (Service service in services)
-            {
-                string color = "green";
-                if (service.LastStartTime == null || !service.Enabled) continue;
-                if (DateTime.Now.Subtract((DateTime)service.Row["HealthTime"]).TotalMinutes > service.HealthMaxMinutes)
-                {
-                    isFailed = true;
-                    color = "red";
-                }
-                AddRow(result, service.Row["Name"].ToString(), string.Format("Health Time: {0},  Max Minutes: {1}", (DateTime)service.Row["HealthTime"], service.HealthMaxMinutes),color);
-            }
-
-            result.Append("</table>");
+            AddStatusRow(rowBuilder, service.Row["Name"].ToString(), isGood, (DateTime)service.Row["HealthTime"], service.HealthMaxMinutes);
         }
 
+        litStatusRows.Text = rowBuilder.ToString();
 
-        isFailed = CheckCount(isFailed, result, 0, "Outbound Email Delay", "There are outgoing email delays of more than 10 minutes, check EmailSender service", @"
+
+        rowBuilder.Clear();
+
+        isFailed = CheckCount(isFailed, rowBuilder, 10, "Outbound Email Delayed", "There are outgoing email delays of more than 10 minutes, check EmailSender service and Socket Labs", @"
 SELECT COUNT(*)
 FROM Emails e 
 WHERE IsWaiting = 1 
@@ -71,38 +43,51 @@ AND e.Attempts < 1
 AND DATEDIFF(MINUTE, e.DateCreated, GETUTCDATE()) > 10
 ");
 
-        isFailed = CheckCount(isFailed, result, 500, "Outbound Email Back Up", "There are too many out going emails pending, check EmailSender service", @"
+        isFailed = CheckCount(isFailed, rowBuilder, 500, "Outbound Email", "There are too many out going emails pending, check EmailSender service", @"
 SELECT COUNT(*)
 FROM Emails e 
 WHERE IsWaiting = 1 
 ");
 
-        isFailed = CheckCount(isFailed, result, 1000, "Email Processing Backed Up", "There are too many records in the EmailPosts table, check EmailProcessor service.", @"
+        isFailed = CheckCount(isFailed, rowBuilder, 1000, "Email Processing", "There are too many records in the EmailPosts table, check EmailProcessor service.", @"
 SELECT COUNT(*) FROM EmailPosts 
 ");
 
-        result.Append(string.Format("<h2 style=\"color:{1};\">{0} is{2} running.</h2>", name, !isFailed ? "green" : "red", !isFailed ? "" : " NOT"));
-        litStatus.Text = result.ToString();
+        //result.Append(string.Format("<h2 style=\"color:{1};\">{0} is{2} running.</h2>", name, !isFailed ? "green" : "red", !isFailed ? "" : " NOT"));
+
+        litQueryRows.Text = rowBuilder.ToString();
+
+        
+        litStatus.Text = !isFailed ? "<div class=\"alert alert-success\">All services are running</div>" : "<div class=\"alert alert-danger\">All services are NOT running</div>";
 
     }
 
-    private static void AddRow(StringBuilder builder, string name, string message, string color)
+    private static void AddStatusRow(StringBuilder builder, string name, bool status, DateTime lastTime, int maxTime)
     {
-        builder.Append(string.Format("<tr style=\"color:{2}\"><td>{0}: </td><td>{1}</td></tr>", name, message, color));
+        builder.Append(string.Format("<tr class=\"{0}\"><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td></tr>", status ? "" : "danger", name, status ? "Good" : "Down", lastTime, maxTime));
+    }
+
+    private static void AddQueryRow(StringBuilder builder, string name, bool status, int max, int count, string message)
+    {
+        builder.Append(string.Format("<tr class=\"{0}\"><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td><td>{5}</td></tr>", status ? "" : "danger", name, status ? "Good" : "Down", max, count, message));
     }
 
     private static bool CheckCount(bool isFailed, StringBuilder builder, int maxCount, string name, string failedMessage, string query)
     {
         int result = (int)SqlExecutor.ExecuteScalar(LoginUser.Anonymous, query);
-        string color = "green";
         bool isBad = false;
         if (result > maxCount)
         {
             isBad = true;
-            color = "red";
+            AddQueryRow(builder, name, false, maxCount, result, failedMessage);
+        }
+        else
+        {
+            string message = failedMessage;
+            AddQueryRow(builder, name, true, maxCount, result, "");
+
         }
 
-        AddRow(builder, name, failedMessage, color);
 
 
         return isFailed || isBad;
