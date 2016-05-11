0be3688a4d02b24a70ae2c32c357657c352ae8c0
kevin4885 <kevin4885@02885c34-e171-44ab-8435-42fb7a3c3aaf>
2016-05-11 15:57:19 +0000

    Added Exception type to logging.
    git-svn-id: https://teamsupport.svn.beanstalkapp.com/main/trunk@4581 02885c34-e171-44ab-8435-42fb7a3c3aaf

--- TeamSupport.Data/Classes/Logs.cs
+++ TeamSupport.Data/Classes/Logs.cs
@@ -9,136 +9,138 @@ using System.IO;
 
 namespace TeamSupport.Data
 {
-  public class Logs
-  {
-    static readonly object _syncObject = new object();
-    string _category = null;
-    string _logPath;
-    string _name;
-
-    public string Category
+    public class Logs
     {
-      get
-      {
-        lock (_syncObject)
+        static readonly object _syncObject = new object();
+        string _category = null;
+        string _logPath;
+        string _name;
+
+        public string Category
         {
-          return _category;
-        } 
-      }
-    }
-
-    public Logs() 
-    { 
-      InitializeLog();
-    }
-
-    public Logs(string category)
-    {
-      this._category = category;
-      InitializeLog();
-    }
-
-    private void InitializeLog()
-    {
-      _name = Path.ChangeExtension(System.AppDomain.CurrentDomain.FriendlyName, "");
-      if (!string.IsNullOrEmpty(Category)) { _name = _name + " [" + Category + "]"; }
-      _logPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Logs");
-      if (!Directory.Exists(_logPath)) Directory.CreateDirectory(_logPath);
-    }
-
-    public void WriteException(Exception ex, DataRow row = null)
-    {
-      WriteEvent("EXCEPTION:");
-      WriteEvent("Message: ");
-      WriteEvent(ex.Message);
-      WriteEvent("Stack Trace: ");
-      WriteEvent(ex.StackTrace);
-      
-      if (row != null) { WriteData(row); }
-
-      if (ex.InnerException != null) WriteException(ex.InnerException);
-    }
-
-    public void WriteData(DataRow row)
-    {
-      WriteEvent("Data Row:");
-      WriteEvent(DataUtils.DataRowToString(row));
-    }
-
-    public void WriteEvent(string message, bool showDateTime)
-    {
-      lock (_syncObject)
-      {
-
-        if (showDateTime) message = string.Format("[{0:hh:mm:ss tt}] {1}", DateTime.Now, message);
-        //TextWriter _writer = TextWriter.Synchronized(!File.Exists(_logPath) ? File.CreateText(_logPath) : File.AppendText(_logPath));
-        
-        string path = Path.Combine(_logPath, string.Format("{0} {1:yyyy-MM-dd}.txt", _name, DateTime.Now));
-
-        bool doesFileExist = File.Exists(path);
-        using (StreamWriter writer = !doesFileExist ? File.CreateText(path) : File.AppendText(path))
-        {
-          writer.WriteLine(message);
+            get
+            {
+                lock (_syncObject)
+                {
+                    return _category;
+                }
+            }
         }
 
-        // Clean up old log files after creating a new on.
-        if (!doesFileExist)
+        public Logs()
         {
-          string[] files = Directory.GetFiles(_logPath, "*.txt");
-
-          foreach (string file in files)
-          {
-            if (File.GetLastWriteTime(file) < DateTime.Now.AddDays(-30)) File.Delete(file);
-          }
-        
+            InitializeLog();
         }
-      }
+
+        public Logs(string category)
+        {
+            this._category = category;
+            InitializeLog();
+        }
+
+        private void InitializeLog()
+        {
+            _name = Path.ChangeExtension(System.AppDomain.CurrentDomain.FriendlyName, "");
+            if (!string.IsNullOrEmpty(Category)) { _name = _name + " [" + Category + "]"; }
+            _logPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Logs");
+            if (!Directory.Exists(_logPath)) Directory.CreateDirectory(_logPath);
+        }
+
+        public void WriteException(Exception ex, DataRow row = null)
+        {
+            WriteEvent("EXCEPTION:");
+            WriteEvent("Message: ");
+            WriteEvent(ex.Message);
+            WriteEvent("Type: ");
+            WriteEvent(ex.GetType().ToString());
+            WriteEvent("Stack Trace: ");
+            WriteEvent(ex.StackTrace);
+
+            if (row != null) { WriteData(row); }
+
+            if (ex.InnerException != null) WriteException(ex.InnerException);
+        }
+
+        public void WriteData(DataRow row)
+        {
+            WriteEvent("Data Row:");
+            WriteEvent(DataUtils.DataRowToString(row));
+        }
+
+        public void WriteEvent(string message, bool showDateTime)
+        {
+            lock (_syncObject)
+            {
+
+                if (showDateTime) message = string.Format("[{0:hh:mm:ss tt}] {1}", DateTime.Now, message);
+                //TextWriter _writer = TextWriter.Synchronized(!File.Exists(_logPath) ? File.CreateText(_logPath) : File.AppendText(_logPath));
+
+                string path = Path.Combine(_logPath, string.Format("{0} {1:yyyy-MM-dd}.txt", _name, DateTime.Now));
+
+                bool doesFileExist = File.Exists(path);
+                using (StreamWriter writer = !doesFileExist ? File.CreateText(path) : File.AppendText(path))
+                {
+                    writer.WriteLine(message);
+                }
+
+                // Clean up old log files after creating a new on.
+                if (!doesFileExist)
+                {
+                    string[] files = Directory.GetFiles(_logPath, "*.txt");
+
+                    foreach (string file in files)
+                    {
+                        if (File.GetLastWriteTime(file) < DateTime.Now.AddDays(-30)) File.Delete(file);
+                    }
+
+                }
+            }
+        }
+
+        public void WriteEventFormat(string formatString, bool showDateTime, params object[] args)
+        {
+            WriteEvent(string.Format(formatString, args), showDateTime);
+        }
+
+        public void WriteEventFormat(string formatString, params object[] args)
+        {
+            WriteEventFormat(formatString, true, args);
+        }
+
+        public void WriteParam(string paramName, string value)
+        {
+            WriteEventFormat("{0}: {1}", paramName, value);
+        }
+        public void WriteEvent(string message)
+
+        {
+            WriteEvent(message, true);
+        }
+
+        public void WriteLine()
+        {
+            WriteEvent(System.Environment.NewLine, false);
+        }
+
+        public void WriteHeader(string text)
+        {
+            StringBuilder builder = new StringBuilder("********************************************");
+            for (int i = 0; i < text.Length + 2; i++)
+            {
+                builder.Append("*");
+            }
+
+            WriteLine();
+            WriteEvent(builder.ToString(), false);
+            WriteEvent(string.Format("********************** {0} **********************", text), false);
+            WriteEvent(builder.ToString(), false);
+            WriteLine();
+
+
+        }
+
+
     }
-
-    public void WriteEventFormat(string formatString, bool showDateTime, params object[] args)
-    {
-      WriteEvent(string.Format(formatString, args), showDateTime);
-    }
-
-    public void WriteEventFormat(string formatString, params object[] args)
-    {
-      WriteEventFormat(formatString, true, args);
-    }
-
-    public void WriteParam(string paramName, string value)
-    {
-      WriteEventFormat("{0}: {1}", paramName, value);
-    }
-    public void WriteEvent(string message)
-    
-    {
-      WriteEvent(message, true);
-    }
-
-    public void WriteLine()
-    {
-      WriteEvent(System.Environment.NewLine, false);
-    }
-
-    public void WriteHeader(string text)
-    {
-      StringBuilder builder = new StringBuilder("********************************************");
-      for (int i = 0; i < text.Length+2; i++)
-			{
-			  builder.Append("*");
-			}
-
-      WriteLine();
-      WriteEvent(builder.ToString(), false);
-      WriteEvent(string.Format("********************** {0} **********************", text), false);
-      WriteEvent(builder.ToString(), false);
-      WriteLine();
-
-
-    }
-
-   
-  }
 }
 
 
