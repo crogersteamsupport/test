9070478a75fc0440723102b31adae960e5590940
vvillagomez <vvillagomez@teamsupport.com>
2016-12-12 20:39:48 -0600

    Sla rework. Set column size of the first column of the table in the Customer Detail / SLA page.

--- WebApp/Resources/Pages/CustomerDetail.js
+++ WebApp/Resources/Pages/CustomerDetail.js
@@ -2464,7 +2464,7 @@ $(document).ready(function () {
                 var html;
 
                 if (!_isParentView && (_mainFrame.Ts.System.User.CanEditCompany || _isAdmin)) {
-                    html = '<td>' + slaTriggers[i].LevelName + '</td><td>' + slaTriggers[i].TicketType + '</td><td>' + slaTriggers[i].Severity + '</td><td>' + slaTriggers[i].SLAType + '</td>';
+                    html = '<td class="col-md-3">' + slaTriggers[i].LevelName + '</td><td>' + slaTriggers[i].TicketType + '</td><td>' + slaTriggers[i].Severity + '</td><td>' + slaTriggers[i].SLAType + '</td>';
                 }
                 else {
                     html = '<td>' + slaTriggers[i].LevelName + '</td><td>' + slaTriggers[i].TicketType + '</td><td>' + slaTriggers[i].Severity + '</td><td>' + slaTriggers[i].SLAType + '</td>';
