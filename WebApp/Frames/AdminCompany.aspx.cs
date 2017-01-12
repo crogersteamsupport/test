94fbb8c6f220cbc4430cfb9a1988902ce052df62
gphillips <gphillips@teamsupport.com>
2017-01-12 11:55:36 -0600

    Reorder require groups option on admin page as per eric request

--- WebApp/Frames/AdminCompany.aspx.cs
+++ WebApp/Frames/AdminCompany.aspx.cs
@@ -173,6 +173,7 @@ public partial class Frames_AdminCompany : BaseFramePage
     table.Rows.Add(new string[] { "Internal SLA:", level == null ? "[None Assigned]" :level.Name});
 
     table.Rows.Add(new string[] { "Show Group Members First in Ticket Assignment List:", organization.ShowGroupMembersFirstInTicketAssignmentList.ToString() });
+    table.Rows.Add(new string[] { "Require Group Assignment On Tickets:", organization.RequireGroupAssignmentOnTickets.ToString() });
     table.Rows.Add(new string[] { "Update Ticket Children Group With Parent:", organization.UpdateTicketChildrenGroupWithParent.ToString() });
     table.Rows.Add(new string[] { "Hide Alert Dismiss for Non Admins:", organization.HideDismissNonAdmins.ToString() });
     if ((organization.ProductType == ProductType.Enterprise || organization.ProductType == ProductType.BugTracking))
@@ -182,7 +183,7 @@ public partial class Frames_AdminCompany : BaseFramePage
 	table.Rows.Add(new string[] { "Two Factor Verification:", organization.TwoStepVerificationEnabled.ToString() });
 	table.Rows.Add(new string[] { "How many days before user passwords expire:", organization.DaysBeforePasswordExpire.ToString() });
 	table.Rows.Add(new string[] { "Do not include attachments on outbound emails:", organization.NoAttachmentsInOutboundEmail.ToString() });
-    table.Rows.Add(new string[] { "Require Group Assignment On Tickets:", organization.RequireGroupAssignmentOnTickets.ToString() });
+
 
         
 
