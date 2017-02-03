4b349921a5da2f5b1d87ac98489f2dd069111207
tpitts <tpitts@teamsupport.com>
2017-02-03 13:03:05 -0700

    open up user profile pic resolution

--- WebApp/App_Code/UserService.cs
+++ WebApp/App_Code/UserService.cs
@@ -178,7 +178,7 @@ namespace TSWebServices
 
             User user = Users.GetUser(TSAuthentication.GetLoginUser(), userID);
 
-				path = String.Format("/dc/{0}/UserAvatar/{1}/73/{2}", user.OrganizationID, userID, Guid.NewGuid()); 
+				path = String.Format("/dc/{0}/UserAvatar/{1}/120/{2}", user.OrganizationID, userID, Guid.NewGuid()); 
             return path;
         }
 
