80b395db5b5f82aa7943589fdc3bf1fa4a4da6d1
vvillagomez <vvillagomez@teamsupport.com>
2017-04-04 16:17:21 -0500

    New Chat. Receive parameters to auto-populate the fields in the init page: fname, lname, email, and msg. Also this is done now in the "chat with us" link of the TS app (except for the msg).

--- WebApp/Resources/Pages/CustomerChatInit.js
+++ WebApp/Resources/Pages/CustomerChatInit.js
@@ -2,6 +2,10 @@
 
 $(document).ready(function () {
     var chatID = Ts.Utils.getQueryValue("uid", window);
+    var fname = Ts.Utils.getQueryValue("fname", window);
+    var lname = Ts.Utils.getQueryValue("lname", window);
+    var email = Ts.Utils.getQueryValue("email", window);
+    var msg = Ts.Utils.getQueryValue("msg", window);
     var chatGuid = { chatGuid: chatID };
 
     IssueAjaxRequest("CheckChatStatus", { chatGuid: chatID },
@@ -49,6 +53,22 @@ $(document).ready(function () {
             });
         }
     });
+
+    if (fname != null && fname != 'undefined') {
+        $("#userFirstName").val(fname);
+    }
+    
+    if (lname != null && lname != 'undefined') {
+        $("#userLastName").val(lname);
+    }
+
+    if (email != null && email != 'undefined') {
+        $("#userEmail").val(email);
+    }
+
+    if (msg != null && msg != 'undefined') {
+        $("#userIssue").val(msg);
+    }
 });
 
 function GetChatSettings(chatID) {
