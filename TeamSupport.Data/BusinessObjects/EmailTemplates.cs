8811555cece3c6d86d472ce7527fcf69763ce2a4
Joe Bannon <jbannon@teamsupport.com>
2017-06-07 16:14:27 -0500

    Applause Email Processor Changes 1

--- TeamSupport.Data/BusinessObjects/EmailTemplates.cs
+++ TeamSupport.Data/BusinessObjects/EmailTemplates.cs
@@ -977,11 +977,13 @@ namespace TeamSupport.Data
             return template.GetMessage();
         }
 
-        public static MailMessage GetReaction(LoginUser loginUser, int ticketID, string hostName)
+        public static MailMessage GetReaction(LoginUser loginUser, User sender, User receiver, int ticketID, string hostName)
         {
-            EmailTemplate template = EmailTemplates.GetEmailTemplate(loginUser,40);
-            string senderName = loginUser.GetUserFullName();
-            template.ReplaceParameter("Sender.Name", senderName).ReplaceParameter("ticketid", ticketID.ToString()).ReplaceParameter("hostname", hostName);
+            EmailTemplate template = EmailTemplates.GetEmailTemplate(loginUser, 40);
+
+            template.ReplaceParameter("sender.name", sender.FirstLastName);
+            template.ReplaceParameter("ticketid", ticketID.ToString());
+            template.ReplaceParameter("hostname", hostName);
             return template.GetMessage();
         }
 
