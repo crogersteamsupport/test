0317de81ca6dcdfc1b54d9e8701e469535dd77a9
Joe Bannon <jbannon@teamsupport.com>
2017-07-26 15:36:05 -0500

    Reboot, Ticket Page, CSS 51

--- WebApp/frontend/javascript/teamsupport.js
+++ WebApp/frontend/javascript/teamsupport.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $('.dropdown-submenu a.test').on("click", function(e){
+    $('.dropdown-submenu a.expand-submenu').on("click", function(e){
         $(this).next('ul').toggle();
         e.stopPropagation();
         e.preventDefault();
-    });
+    }); 
 });
