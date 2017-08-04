24c32c00b754173524f334a5b26f757e56e8431c
lperezpicasso <lperezpicasso@teamsupport.com>
2017-08-04 15:55:40 -0500

    Format RestUsers file

--- TeamSupport.Api/RestData/RestUsers.cs
+++ TeamSupport.Api/RestData/RestUsers.cs
@@ -10,104 +10,104 @@ using System.Net;
 
 namespace TeamSupport.Api
 {
-  public class RestUsers
-  {
-
-    public static string GetUser(RestCommand command, int userID)
-    {
-      UsersViewItem item = UsersView.GetUsersViewItem(command.LoginUser, userID);
-      if (item.OrganizationID != command.Organization.OrganizationID) throw new RestException(HttpStatusCode.Unauthorized);
-
-      return item.GetXml("User", true);
-    }
-
-    public static string GetUsers(RestCommand command, bool orderByDateCreated = false)
-    {
-      UsersView items = new UsersView(command.LoginUser);
-      if (orderByDateCreated)
-      {
-        items.LoadByOrganizationID(command.Organization.OrganizationID, false, "DateCreated DESC");      
-      }
-      else
-      {
-        items.LoadByOrganizationID(command.Organization.OrganizationID, false);
-      }
-      return items.GetXml("Users", "User", true, command.Filters);
-    }
-
-    public static string CreateUser(RestCommand command)
+    public class RestUsers
     {
 
-      if (Organizations.GetUserCount(command.LoginUser, command.LoginUser.OrganizationID) >= command.Organization.UserSeats)
-      {
-        throw new RestException(HttpStatusCode.Forbidden, "You are already at your maximum user seat count for your license.");
-      }
-      Addresses addresses = new Addresses(command.LoginUser);
-      Address address = addresses.AddNewAddress();
+        public static string GetUser(RestCommand command, int userID)
+        {
+            UsersViewItem item = UsersView.GetUsersViewItem(command.LoginUser, userID);
+            if (item.OrganizationID != command.Organization.OrganizationID) throw new RestException(HttpStatusCode.Unauthorized);
 
-      PhoneNumbers phoneNumbers = new PhoneNumbers(command.LoginUser);
-      PhoneNumber phoneNumber = phoneNumbers.AddNewPhoneNumber();
+            return item.GetXml("User", true);
+        }
 
-      Users users = new Users(command.LoginUser);
-      User user = users.AddNewUser();
-      user.FullReadFromXml(command.Data, true, ref phoneNumber, ref address);
-      user.OrganizationID = command.LoginUser.OrganizationID;
-      user.ActivatedOn = DateTime.UtcNow;
-      user.LastLogin = DateTime.UtcNow.AddHours(-1);
-      user.LastActivity = DateTime.UtcNow.AddHours(-1);
-      user.EnforceSingleSession = true;
-      user.NeedsIndexing = true;
-      user.Collection.Save();
-      user.UpdateCustomFieldsFromXml(command.Data);
+        public static string GetUsers(RestCommand command, bool orderByDateCreated = false)
+        {
+            UsersView items = new UsersView(command.LoginUser);
+            if (orderByDateCreated)
+            {
+                items.LoadByOrganizationID(command.Organization.OrganizationID, false, "DateCreated DESC");
+            }
+            else
+            {
+                items.LoadByOrganizationID(command.Organization.OrganizationID, false);
+            }
+            return items.GetXml("Users", "User", true, command.Filters);
+        }
 
-      if (!String.IsNullOrEmpty(phoneNumber.Number) || !String.IsNullOrEmpty(phoneNumber.Extension))
-      {
-        phoneNumber.RefType = ReferenceType.Users;
-        phoneNumber.RefID = user.UserID;
-        phoneNumbers.Save();
-      }
+        public static string CreateUser(RestCommand command)
+        {
 
-      if (!String.IsNullOrEmpty(address.Description)
-      || !String.IsNullOrEmpty(address.Addr1)
-      || !String.IsNullOrEmpty(address.Addr2)
-      || !String.IsNullOrEmpty(address.Addr3)
-      || !String.IsNullOrEmpty(address.City)
-      || !String.IsNullOrEmpty(address.Country)
-      || !String.IsNullOrEmpty(address.Description)
-      || !String.IsNullOrEmpty(address.State)
-      || !String.IsNullOrEmpty(address.Zip))
-      {
-        address.RefType = ReferenceType.Users;
-        address.RefID = user.UserID;
-        addresses.Save();
-      }
+            if (Organizations.GetUserCount(command.LoginUser, command.LoginUser.OrganizationID) >= command.Organization.UserSeats)
+            {
+                throw new RestException(HttpStatusCode.Forbidden, "You are already at your maximum user seat count for your license.");
+            }
+            Addresses addresses = new Addresses(command.LoginUser);
+            Address address = addresses.AddNewAddress();
 
-      return UsersView.GetUsersViewItem(command.LoginUser, user.UserID).GetXml("User", true);
+            PhoneNumbers phoneNumbers = new PhoneNumbers(command.LoginUser);
+            PhoneNumber phoneNumber = phoneNumbers.AddNewPhoneNumber();
+
+            Users users = new Users(command.LoginUser);
+            User user = users.AddNewUser();
+            user.FullReadFromXml(command.Data, true, ref phoneNumber, ref address);
+            user.OrganizationID = command.LoginUser.OrganizationID;
+            user.ActivatedOn = DateTime.UtcNow;
+            user.LastLogin = DateTime.UtcNow.AddHours(-1);
+            user.LastActivity = DateTime.UtcNow.AddHours(-1);
+            user.EnforceSingleSession = true;
+            user.NeedsIndexing = true;
+            user.Collection.Save();
+            user.UpdateCustomFieldsFromXml(command.Data);
+
+            if (!String.IsNullOrEmpty(phoneNumber.Number) || !String.IsNullOrEmpty(phoneNumber.Extension))
+            {
+                phoneNumber.RefType = ReferenceType.Users;
+                phoneNumber.RefID = user.UserID;
+                phoneNumbers.Save();
+            }
+
+            if (!String.IsNullOrEmpty(address.Description)
+            || !String.IsNullOrEmpty(address.Addr1)
+            || !String.IsNullOrEmpty(address.Addr2)
+            || !String.IsNullOrEmpty(address.Addr3)
+            || !String.IsNullOrEmpty(address.City)
+            || !String.IsNullOrEmpty(address.Country)
+            || !String.IsNullOrEmpty(address.Description)
+            || !String.IsNullOrEmpty(address.State)
+            || !String.IsNullOrEmpty(address.Zip))
+            {
+                address.RefType = ReferenceType.Users;
+                address.RefID = user.UserID;
+                addresses.Save();
+            }
+
+            return UsersView.GetUsersViewItem(command.LoginUser, user.UserID).GetXml("User", true);
+        }
+
+        public static string UpdateUser(RestCommand command, int userID)
+        {
+            User user = Users.GetUser(command.LoginUser, userID);
+            if (user == null) throw new RestException(HttpStatusCode.BadRequest);
+            Organization organization = Organizations.GetOrganization(command.LoginUser, user.OrganizationID);
+            if (organization.OrganizationID != command.Organization.OrganizationID) throw new RestException(HttpStatusCode.Unauthorized);
+            user.ReadFromXml(command.Data, false);
+            user.Collection.Save();
+            user.UpdateCustomFieldsFromXml(command.Data);
+            return UsersView.GetUsersViewItem(command.LoginUser, user.UserID).GetXml("User", true);
+        }
+
+        public static string DeleteUser(RestCommand command, int userID)
+        {
+            UsersViewItem user = UsersView.GetUsersViewItem(command.LoginUser, userID);
+            if (user == null) throw new RestException(HttpStatusCode.BadRequest);
+            Organization organization = Organizations.GetOrganization(command.LoginUser, user.OrganizationID);
+            if (organization.OrganizationID != command.Organization.OrganizationID) throw new RestException(HttpStatusCode.Unauthorized);
+            string result = user.GetXml("User", true);
+            Users.MarkUserDeleted(command.LoginUser, userID);
+            return result;
+        }
+
+
     }
-
-    public static string UpdateUser(RestCommand command, int userID)
-    {
-      User user = Users.GetUser(command.LoginUser, userID);
-      if (user == null) throw new RestException(HttpStatusCode.BadRequest);
-      Organization organization = Organizations.GetOrganization(command.LoginUser, user.OrganizationID);
-      if (organization.OrganizationID != command.Organization.OrganizationID) throw new RestException(HttpStatusCode.Unauthorized);
-      user.ReadFromXml(command.Data, false);
-      user.Collection.Save();
-      user.UpdateCustomFieldsFromXml(command.Data);
-      return UsersView.GetUsersViewItem(command.LoginUser, user.UserID).GetXml("User", true);
-    }
-
-    public static string DeleteUser(RestCommand command, int userID)
-    {
-      UsersViewItem user = UsersView.GetUsersViewItem(command.LoginUser, userID);
-      if (user == null) throw new RestException(HttpStatusCode.BadRequest);
-      Organization organization = Organizations.GetOrganization(command.LoginUser, user.OrganizationID);
-      if (organization.OrganizationID != command.Organization.OrganizationID) throw new RestException(HttpStatusCode.Unauthorized);
-      string result = user.GetXml("User", true);
-      Users.MarkUserDeleted(command.LoginUser, userID);
-      return result;
-    }
-
-
-  }
 }
