a3596f6f548d943633e7031d71eb012103d2912f
lperezpicasso <lperezpicasso@teamsupport.com>
2017-09-26 11:45:07 -0500

    Ticket[45431]: Code restored after merge conflict.

--- TeamSupport.Data/BusinessObjects/ProductFamilies.cs
+++ TeamSupport.Data/BusinessObjects/ProductFamilies.cs
@@ -228,6 +228,30 @@ namespace TeamSupport.Data
 				Fill(command);
 			}
 		}
+
+        public ProductFamily FindByName(string name)
+        {
+            foreach (ProductFamily productFamily in this)
+            {
+                if (productFamily.Name.Trim().ToLower() == name.Trim().ToLower())
+                {
+                    return productFamily;
+                }
+            }
+            return null;
+        }
+
+        public ProductFamily FindByImportID(string importID)
+        {
+            foreach (ProductFamily productFamily in this)
+            {
+                if (productFamily.ImportID.Trim().ToLower() == importID.Trim().ToLower())
+                {
+                    return productFamily;
+                }
+            }
+            return null;
+        }
 	}
 
 }
