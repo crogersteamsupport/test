3130329678f9792e19bee72e46be110218bd9d64
Scot Prichard <sprichard@teamsupport.com>
2018-04-26 15:30:27 -0500

    optimize performance of DB query

--- CDI2/TicketReader.cs
+++ CDI2/TicketReader.cs
@@ -20,38 +20,67 @@ namespace TeamSupport.CDI
     class TicketReader
     {
         DateRange _dateRange;
-        public TicketJoin[] AllTickets { get; private set; }  // tickets for organization in the last year
+        private TicketJoin[] _tickets;
+        public TicketJoin[] AllTickets { get { return _tickets; } }  // tickets for organization in the last year
 
         /// <summary>Time frame to analyze the ticket data</summary>
         /// <param name="analysisInterval"></param>
         public TicketReader(DateRange analysisInterval)
         {
             _dateRange = analysisInterval;
-            AllTickets = null;
+            _tickets = null;
         }
 
         /// <summary> Load the tickets since the start date </summary>
         public void LoadAllTickets()
         {
-            if (AllTickets != null)
-                return;
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
             try
             {
                 string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 using (DataContext db = new DataContext(connection))
                 {
-                    AllTickets = LoadAllTicketsHelper(db);
+                    // Verified counts by the following query:
+                    //SELECT DISTINCT Count([TicketID])
+                    //  FROM [dbo].[Tickets] as t
+                    //  JOIN [dbo].[TicketTypes] as tt on t.TicketTypeID=tt.TicketTypeID
+                    //  JOIN [dbo].[TicketStatuses] as ts on t.TicketStatusID=ts.TicketStatusID
+                    //  WHERE t.DateCreated > '2013-04-29 00:00:00' AND (t.TicketSource != 'SalesForce') AND
+                    //  ((ts.IsClosed=0) OR (t.[DateClosed] > t.[DateCreated]))
+
+                    Table<Ticket> ticketsTable = db.GetTable<Ticket>();
+                    Table<TicketStatus> ticketStatusesTable = db.GetTable<TicketStatus>();
+                    Table<TicketType> ticketTypesTable = db.GetTable<TicketType>();
+                    Table<Action> actionsTable = db.GetTable<Action>();
+                    var query = (from t in ticketsTable
+                                 join tt in ticketTypesTable on t.TicketTypeID equals tt.TicketTypeID
+                                 join ts in ticketStatusesTable on t.TicketStatusID equals ts.TicketStatusID
+                                 where (t.DateCreated > _dateRange.StartDate) &&
+                                     (!ts.IsClosed || (t.DateClosed.Value > t.DateCreated)) &&
+                                     (t.TicketSource != "SalesForce") &&    // ignore imported tickets
+                                     (!tt.ExcludeFromCDI) &&
+                                     (!ts.ExcludeFromCDI)
+                                 orderby t.OrganizationID
+                                 select new TicketJoin()
+                                 {
+                                     TicketID = t.TicketID,
+                                     TicketStatusName = ts.Name,
+                                     TicketTypeName = tt.Name,
+                                     OrganizationID = t.OrganizationID,
+                                     DateClosed = t.DateClosed,
+                                     TicketSource = t.TicketSource,
+                                     DateCreated = t.DateCreated,
+                                     IsClosed = ts.IsClosed,
+                                     ActionsCount = (from t in ticketsTable join a in actionsTable on t.TicketID equals a.TicketID select a.ActionID).Count()
+                                 }).Distinct();
+
+                    _tickets = query.ToArray();
                 }
             }
             catch (Exception e)
             {
                 CDIEventLog.WriteEntry("Ticket Read failed", e);
             }
-            long ellapsed = stopwatch.ElapsedMilliseconds;
         }
 
         /// <summary>Extract the tickets for this organization</summary>
@@ -59,77 +88,29 @@ namespace TeamSupport.CDI
         /// <returns></returns>
         public TicketJoin[] Read(int organizationID)
         {
-            LoadAllTickets(); // make sure we are initilized
             var query = AllTickets.Where(t => t.OrganizationID == organizationID);//.OrderBy(t => t.DateCreated);
             return query.ToArray();
         }
 
         public int[] ReadOrganizationIDs()
         {
-            LoadAllTickets(); // make sure we are initilized
             var query = AllTickets.Select(t => t.OrganizationID).Distinct();
             return query.ToArray();
         }
 
-        /// <summary> big data - Load the tickets in pages </summary>
-        /// <param name="startDate"></param>
-        /// <param name="db"></param>
-        TicketJoin[] LoadAllTicketsHelper(DataContext db)
+        public List<int> ReadOrganizationOffsets()
         {
-            TicketJoin[] tickets = null;
-
-            // tables
-            Table<Ticket> ticketsTable = db.GetTable<Ticket>();
-            Table<TicketStatus> ticketStatusesTable = db.GetTable<TicketStatus>();
-            Table<TicketType> ticketTypesTable = db.GetTable<TicketType>();
-            Table<Action> actionsTable = db.GetTable<Action>();
-
-            // loop through loading blocks
-            DateTime queryDate = _dateRange.StartDate;
-            while (queryDate < _dateRange.EndDate)
+            List<int> results = new List<int>();
+            int organizationID = Int32.MinValue;
+            for(int i = 0; i < _tickets.Length; ++i)
             {
-                var query = (from t in ticketsTable
-                             join tt in ticketTypesTable on t.TicketTypeID equals tt.TicketTypeID
-                             join ts in ticketStatusesTable on t.TicketStatusID equals ts.TicketStatusID
-                             join a in actionsTable on t.TicketID equals a.TicketID into actionJoin // count actions
-                             where (t.DateCreated > queryDate) &&
-                                 //(ts.IsClosed == true) &&   // only closed tickets
-                                 //(t.DateClosed.HasValue) &&
-                                 //(t.DateClosed.Value > t.DateCreated.AddSeconds(1)) &&  // ignore those open for less than a second
-                                 ((ts.IsClosed == false) || ((t.DateClosed.HasValue) && (t.DateClosed.Value > t.DateCreated.AddSeconds(1)))) && // open for more than 1 second
-                                 (t.TicketSource != "SalesForce") &&    // ignore imported tickets
-                                 (!tt.ExcludeFromCDI) && 
-                                 (!ts.ExcludeFromCDI)
-                             orderby t.DateCreated
-                             select new TicketJoin()
-                             {
-                                 TicketID = t.TicketID,
-                                 TicketStatusName = ts.Name,
-                                 TicketTypeName = tt.Name,
-                                 OrganizationID = t.OrganizationID,
-                                 DateClosed = t.DateClosed,
-                                 TicketSource = t.TicketSource,
-                                 DateCreated = t.DateCreated,
-                                 ActionsCount = actionJoin.Count(),
-                                 IsClosed = ts.IsClosed
-                             }).Distinct();
+                if (_tickets[i].OrganizationID == organizationID)
+                    continue;
 
-                // execute the query into the array
-                TicketJoin[] queryResults = query.ToArray();
-                if (queryResults.Length == 0)
-                    break;  // done - no more records
-
-                if (tickets == null)
-                    tickets = queryResults;    // first call
-                else
-                {
-                    int previousLength = tickets.Length;
-                    Array.Resize(ref tickets, tickets.Length + queryResults.Length);
-                    queryResults.CopyTo(tickets, previousLength);
-                }
-                queryDate = tickets[tickets.Length - 1].DateCreated;
+                organizationID = _tickets[i].OrganizationID;
+                results.Add(organizationID);
             }
-            return tickets;
+            return results;
         }
     }
 }
