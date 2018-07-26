ce579519b844b986190ad0690ae50017b6b52782
Scot Prichard <sprichard@teamsupport.com>
2018-07-26 15:03:57 -0500

    add Customer and Client

---
+++ TeamSupport.Model/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSupport.Data.Model
+{
+    class Client : UserSession
+    {
+        public Client(OrganizationModel organization, int userID) : base(organization, userID)
+        {
+        }
+    }
+}
---
+++ TeamSupport.Model/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSupport.Data.Model
+{
+    class Customer : UserSession
+    {
+        public Customer(OrganizationModel organization, int userID) : base(organization, userID)
+        {
+        }
+    }
+}
