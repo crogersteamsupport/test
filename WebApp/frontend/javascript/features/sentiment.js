ce5347b6c07f35eb1a52188267d8b22b18232674
Joe Bannon <jbannon@teamsupport.com>
2018-07-30 15:55:27 -0500

    Saving update.

--- WebApp/frontend/javascript/features/sentiment.js
+++ WebApp/frontend/javascript/features/sentiment.js
@@ -116,319 +116,335 @@ $(document).on('click', '#ticket-Sentiment', function(e) {
     // BRANCHES, START @ TOP LEFT.
 
     // BRANCH, TOP LEFT.
+    if (sentimentData.Frustrated) {
+        ctx.beginPath();
+        ctx.moveTo(100, 100);
+        ctx.lineTo(200, 200);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(229,99,66,0.7)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(100, 100);
-    ctx.lineTo(200, 200);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(229,99,66,0.7)";
-    ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(100, 100, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(229,99,66,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(229,99,66,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.arc(100, 100, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(229,99,66,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(229,99,66,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.font = "13px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillText("Frustrated", 100, 150);
 
-    ctx.beginPath();
-    ctx.font = "13px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(0,0,0,0.8)";
-    ctx.fillText("Frustrated", 100, 150);
+        ctx.beginPath();
+        ctx.font = "12px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(229,99,66,1)";
+        ctx.fillText(sentimentData.Frustrated, 100, 170);
 
-    ctx.beginPath();
-    ctx.font = "12px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(229,99,66,1)";
-    ctx.fillText("+10", 100, 170);
-
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("-", 100, 99);
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("-", 100, 99);
+    }
 
     // BRANCH, TOP LEFT-CENTER.
 
-    ctx.beginPath();
-    ctx.moveTo(250, 100);
-    ctx.lineTo(350, 200);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(237,167,42,0.7)";
-    ctx.stroke();
+    if (sentimentData.Impolite) {
+        ctx.beginPath();
+        ctx.moveTo(250, 100);
+        ctx.lineTo(350, 200);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(237,167,42,0.7)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(250, 100, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(237,167,42,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(237,167,42,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(250, 100, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(237,167,42,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(237,167,42,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.font = "13px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(0,0,0,0.8)";
-    ctx.fillText("Impolite", 250, 150);
+        ctx.beginPath();
+        ctx.font = "13px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillText("Impolite", 250, 150);
 
-    ctx.beginPath();
-    ctx.font = "12px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(237,167,42,1)";
-    ctx.fillText("-10", 250, 170);
+        ctx.beginPath();
+        ctx.font = "12px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(237,167,42,1)";
+        ctx.fillText(sentimentData.Impolite, 250, 170);
 
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("-", 250, 99);
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("-", 250, 99);
+    }
 
     // BRANCH, TOP RIGHT-CENTER.
 
-    ctx.beginPath();
-    ctx.moveTo(450, 100);
-    ctx.lineTo(350, 200);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(237,167,42,0.7)";
-    ctx.stroke();
+    if (sentimentData.Polite) {
+        ctx.beginPath();
+        ctx.moveTo(450, 100);
+        ctx.lineTo(350, 200);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(237,167,42,0.7)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(450, 100, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(237,167,42,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(237,167,42,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(450, 100, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(237,167,42,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(237,167,42,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.font = "13px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(0,0,0,0.8)";
-    ctx.fillText("Polite", 450, 150);
+        ctx.beginPath();
+        ctx.font = "13px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillText("Polite", 450, 150);
 
-    ctx.beginPath();
-    ctx.font = "12px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(237,167,42,1)";
-    ctx.fillText("+10", 450, 170);
+        ctx.beginPath();
+        ctx.font = "12px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(237,167,42,1)";
+        ctx.fillText('+' + sentimentData.Polite, 450, 170);
 
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("+", 450, 99);
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("+", 450, 99);
+    }
 
     // BRANCH, TOP RIGHT.
 
-    ctx.beginPath();
-    ctx.moveTo(500, 200);
-    ctx.lineTo(600, 100);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(122,215,20,0.7)";
-    ctx.stroke();
+    if (sentimentData.Excited) {
+        ctx.beginPath();
+        ctx.moveTo(500, 200);
+        ctx.lineTo(600, 100);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(122,215,20,0.7)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(600, 100, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(122,215,20,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(122,215,20,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(600, 100, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(122,215,20,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(122,215,20,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.font = "13px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(0,0,0,0.8)";
-    ctx.fillText("Excited", 600, 150);
+        ctx.beginPath();
+        ctx.font = "13px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillText("Excited", 600, 150);
 
-    ctx.beginPath();
-    ctx.font = "12px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(122,215,20,1)";
-    ctx.fillText("+10", 600, 170);
+        ctx.beginPath();
+        ctx.font = "12px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(122,215,20,1)";
+        ctx.fillText('+' + sentimentData.Excited, 600, 170);
 
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("+", 600, 99);
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("+", 600, 99);
+    }
 
     // BRANCH CIRCLE, BOTTOM RIGHT.
 
-    ctx.beginPath();
-    ctx.moveTo(500, 200);
-    ctx.lineTo(600, 300);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(122,215,20,0.7)";
-    ctx.stroke();
+    if (sentimentData.Satisfied) {
+        ctx.beginPath();
+        ctx.moveTo(500, 200);
+        ctx.lineTo(600, 300);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(122,215,20,0.7)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(600, 300, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(122,215,20,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(122,215,20,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(600, 300, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(122,215,20,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(122,215,20,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.font = "13px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(0,0,0,0.8)";
-    ctx.fillText("Satisfied", 600, 350);
+        ctx.beginPath();
+        ctx.font = "13px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillText("Satisfied", 600, 350);
 
-    ctx.beginPath();
-    ctx.font = "12px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(122,215,20,1)";
-    ctx.fillText("+10", 600, 370);
+        ctx.beginPath();
+        ctx.font = "12px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(122,215,20,1)";
+        ctx.fillText('+' + sentimentData.Satisfied, 600, 370);
 
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("+", 600, 299);
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("+", 600, 299);
+    }
 
     // BRANCH CIRCLE, BOTTOM LEFT-CENTER.
 
-    ctx.beginPath();
-    ctx.moveTo(250, 300);
-    ctx.lineTo(350, 200);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(237,167,42,0.7)";
-    ctx.stroke();
+    if (sentimentData.Sympathetic) {
+        ctx.beginPath();
+        ctx.moveTo(250, 300);
+        ctx.lineTo(350, 200);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(237,167,42,0.7)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(250, 300, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(237,167,42,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(237,167,42,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(250, 300, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(237,167,42,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(237,167,42,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.font = "13px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(0,0,0,0.8)";
-    ctx.fillText("Sympathetic", 250, 350);
+        ctx.beginPath();
+        ctx.font = "13px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillText("Sympathetic", 250, 350);
 
-    ctx.beginPath();
-    ctx.font = "12px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(237,167,42,1)";
-    ctx.fillText("-10", 250, 370);
+        ctx.beginPath();
+        ctx.font = "12px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(237,167,42,1)";
+        ctx.fillText(sentimentData.Sympathetic, 250, 370);
 
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("-", 250, 299);
-
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("-", 250, 299);
+    }
 
     // BRANCH, BOTTOM LEFT.
 
-    ctx.beginPath();
-    ctx.moveTo(100, 300);
-    ctx.lineTo(200, 200);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(229,99,66,0.7)";
-    ctx.stroke();
+    if (sentimentData.Sad) {
+        ctx.beginPath();
+        ctx.moveTo(100, 300);
+        ctx.lineTo(200, 200);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(229,99,66,0.7)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(100, 300, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(229,99,66,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(229,99,66,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(100, 300, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(229,99,66,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(229,99,66,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.font = "13px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(0,0,0,0.8)";
-    ctx.fillText("Sad", 100, 350);
+        ctx.beginPath();
+        ctx.font = "13px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillText("Sad", 100, 350);
 
-    ctx.beginPath();
-    ctx.font = "12px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(229,99,66,1)";
-    ctx.fillText("-10", 100, 370);
+        ctx.beginPath();
+        ctx.font = "12px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(229,99,66,1)";
+        ctx.fillText(sentimentData.Sad, 100, 370);
 
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("-", 100, 299);
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("-", 100, 299);
+    }
 
     // LEFT CIRCLE.
 
-    ctx.beginPath();
-    ctx.moveTo(200, 200);
-    ctx.lineTo(350, 200);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(229,99,66,0.7)";
-    ctx.stroke();
+    if (sentimentData.Frustrated || sentimentData.Sad) {
+        ctx.beginPath();
+        ctx.moveTo(200, 200);
+        ctx.lineTo(350, 200);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(229,99,66,0.7)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(200, 200, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(229,99,66,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(229,99,66,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(200, 200, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(229,99,66,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(229,99,66,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("-", 200, 199);
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("-", 200, 199);
+    }
 
     // RIGHT CIRCLE.
 
-    ctx.beginPath();
-    ctx.moveTo(350, 200);
-    ctx.lineTo(500, 200);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgba(122,215,20,1)";
-    ctx.stroke();
+    if (sentimentData.Excited || sentimentData.Satisfied) {
+        ctx.beginPath();
+        ctx.moveTo(350, 200);
+        ctx.lineTo(500, 200);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(122,215,20,1)";
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(500, 200, 20, 0, 2 * Math.PI);
-    ctx.strokeStyle = "rgba(122,215,20,0.5)";
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fillStyle = "rgba(122,215,20,1)";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(500, 200, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "rgba(122,215,20,0.5)";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = "rgba(122,215,20,1)";
+        ctx.fill();
 
-    ctx.beginPath();
-    ctx.font = "20px Verdana";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgba(255,255,255,0.8)";
-    ctx.fillText("+", 500, 199);
+        ctx.beginPath();
+        ctx.font = "20px Verdana";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "rgba(255,255,255,0.8)";
+        ctx.fillText("+", 500, 199);
+    }
 
     // CENTER CIRCLE.
 
@@ -454,5 +470,38 @@ $(document).on('click', '#ticket-Sentiment', function(e) {
     ctx.fillStyle = "#7ad714";
     ctx.fillText("78 / 100", 350, 315);
 
+    // PROGRESS BAR.
+
+    ctx.beginPath();
+    ctx.moveTo(100, 400);
+    ctx.lineTo(600, 400);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "rgba(0,0,0,0.2)";
+    ctx.stroke();
+
+    if (sentimentData.TicketSentimentScore > 50) {
+        var distance = (sentimentData.TicketSentimentScore - 50) * 5 + 350;
+        ctx.beginPath();
+        ctx.moveTo(350, 400);
+        ctx.lineTo(distance, 400);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(122,215,20,0.9)";
+        ctx.stroke();
+    } else if (sentimentData.TicketSentimentScore < 50) {
+        var distance = 350 - (50 - sentimentData.TicketSentimentScore) * 5;
+        ctx.beginPath();
+        ctx.moveTo(350, 400);
+        ctx.lineTo(distance, 400);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "rgba(229,99,66,0.9)";
+        ctx.stroke();
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(348, 400);
+    ctx.lineTo(352, 400);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "rgba(0,0,0,0.6)";
+    ctx.stroke();
 
 });
