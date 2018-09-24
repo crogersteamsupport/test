99c155f3e035c5f798cc769d1d93984dfe65ac75
Joe Bannon <jbannon@teamsupport.com>
2018-09-24 13:34:46 -0500

    Update hydrate button to use proper org id.

--- WebApp/Resources/Utils/React/RubbishKevinCode/React.js
+++
@@ -1,4 +0,0 @@
-﻿ReactDOM.render(
-    <h1>This is where all of Kevin's broken code goes... </h1>,
-    document.getElementById('root')
-);
--- WebApp/frontend/javascript/features/deflector.js
+++ WebApp/frontend/javascript/features/deflector.js
@@ -1,6 +1,7 @@
 $(document).on('click', '.action-hydrate-deflector', function(e) {
     e.stopPropagation();
     e.preventDefault();
-    window.parent.Ts.Services.Deflector.HydrateOrganization(1078, function(result) {
+    window.parent.Ts.Services.Deflector.HydrateOrganization(selectedOrgId, function(result) {
+        console.log(result);
     });
 });
