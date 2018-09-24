291c6e6303a1ceafba63b81a4aaa2be6e8a8b175
tpitts <tpitts@teamsupport.com>
2018-09-24 14:57:22 -0500

    added organizationID back

--- TeamSupport.Data/BusinessObjects/Deflector.cs
+++ TeamSupport.Data/BusinessObjects/Deflector.cs
@@ -10,75 +10,58 @@ using Newtonsoft.Json;
 using System.Data.Linq;
 using TeamSupport.Data.BusinessObjects;
 
-namespace TeamSupport.Data {
+namespace TeamSupport.Data
+{
 
-    public partial class Deflector {
+    public partial class Deflector
+    {
 
-        public static string PullOrganization (LoginUser loginUser, int organizationID) {
-            try {
-                //using (SqlConnection connection = new SqlConnection(loginUser.ConnectionString)) {
-                //    using (SqlCommand command = new SqlCommand()) {
-                //        command.Connection   = connection;
-                //        command.CommandType  = CommandType.Text;
-                //        command.CommandText  = "SELECT tickets.TicketID, tickets.Name, tickets.OrganizationID, tickets.ProductID, tickets.IsVisibleOnPortal, tags.Value FROM dbo.tickets AS Tickets ";
-                //        command.CommandText += "INNER JOIN dbo.TagLinks AS TagLinks ON TagLinks.RefType = 17 AND TagLinks.RefID = Tickets.TicketID ";
-                //        command.CommandText += "INNER JOIN dbo.Tags AS Tags ON Tags.TagID = TagLinks.TagID ";
-                //        command.CommandText += "WHERE tickets.organizationID = @organizationID and tickets.IsKnowledgeBase = 1 ";
-                //        command.CommandText += "FOR JSON PATH, ROOT('results')";
-                //        command.Parameters.AddWithValue("@organizationID", organizationID);
-                //        connection.Open();
-                //        SqlDataReader reader = command.ExecuteReader();
-                //        if (reader.HasRows && reader.Read()) {
-                //            return reader.GetValue(0).ToString();
-                //        } else {
-                //            return "nothing";
-                //        }
-                //    }
-                //}
-
+        public static string PullOrganization(LoginUser loginUser, int organizationID)
+        {
+            try
+            {
                 using (SqlConnection connection = new SqlConnection(loginUser.ConnectionString))
                 {
-                    var query = @"Select 
-	                            tickets.TicketID,
-	                            tickets.Name,
-	                            tickets.OrganizationID,
-	                            tickets.ProductID,
-	                            tickets.IsVisibleOnPortal,
-	                            tags.Value
-                              From dbo.tickets as Tickets
-	                            inner join dbo.TagLinks as TagLinks
-		                            on TagLinks.RefType = 17 and TagLinks.RefID = tickets.TicketID
-	                            inner join dbo.Tags as Tags
-		                            on Tags.TagID = TagLinks.TagID
-                              Where tickets.IsKnowledgeBase = 1
-                              For JSON Auto";
-                    var cmd = new SqlCommand(query, connection);
-                    connection.Open();
-
-                    var jsonResult = new StringBuilder();
-                    SqlDataReader reader = cmd.ExecuteReader();
-
-                    if (!reader.HasRows)
+                    using (SqlCommand command = new SqlCommand())
                     {
-                        jsonResult.Append("[]");
-                    }
-                    else
-                    {
-                        while (reader.Read())
+                        command.Connection = connection;
+                        command.CommandType = CommandType.Text;
+                        command.CommandText = "SELECT tickets.TicketID, tickets.Name, tickets.OrganizationID, tickets.ProductID, tickets.IsVisibleOnPortal, tags.Value FROM dbo.tickets AS Tickets ";
+                        command.CommandText += "INNER JOIN dbo.TagLinks AS TagLinks ON TagLinks.RefType = 17 AND TagLinks.RefID = Tickets.TicketID ";
+                        command.CommandText += "INNER JOIN dbo.Tags AS Tags ON Tags.TagID = TagLinks.TagID ";
+                        command.CommandText += "WHERE tickets.organizationID = @organizationID and tickets.IsKnowledgeBase = 1 ";
+                        command.CommandText += "FOR JSON Auto";
+
+                        command.Parameters.AddWithValue("@organizationID", organizationID);
+                        connection.Open();
+
+                        var jsonResult = new StringBuilder();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        if (!reader.HasRows)
                         {
-                            jsonResult.Append(reader.GetValue(0).ToString());
+                            jsonResult.Append("[]");
                         }
-                    }
+                        else
+                        {
+                            while (reader.Read())
+                            {
+                                jsonResult.Append(reader.GetValue(0).ToString());
+                            }
+                        }
 
-                    reader.Close();
-                    return jsonResult.ToString();
+                        reader.Close();
+                        return jsonResult.ToString();
+                    }
                 }
             }
-            catch (SqlException e) {
+            catch (SqlException e)
+            {
                 return "error";
                 // return e.Message;
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 return "error";
             }
 
