d83e55b85ff0d24c2f8ca0ce00a14d5b4badd8b5
Raquel Forester <rforester@teamsupport.com>
2018-10-02 14:46:19 -0500

    Removed rownum from exports

--- TeamSupport.Data/BusinessObjects/Reports.cs
+++ TeamSupport.Data/BusinessObjects/Reports.cs
@@ -580,7 +580,7 @@ namespace TeamSupport.Data
         private static void GetTabularSqlForExports(LoginUser loginUser, SqlCommand command, TabularReport tabularReport, bool inlcudeHiddenFields, bool isSchemaOnly, int? reportID, bool useUserFilter, string sortField = null, string sortDir = null)
         {
             StringBuilder builder = new StringBuilder();
-            GetTabluarSelectClause(loginUser, command, builder, tabularReport, inlcudeHiddenFields, isSchemaOnly, sortField, sortDir);
+            GetTabluarSelectClauseForExport(loginUser, command, builder, tabularReport, inlcudeHiddenFields, isSchemaOnly, sortField, sortDir);
             if (isSchemaOnly)
             {
                 command.CommandText = builder.ToString();
@@ -813,6 +813,177 @@ namespace TeamSupport.Data
 
             if (isSchemaOnly) builder.Append(" AND (0=1)");
         }
+
+        private static void GetTabluarSelectClauseForExport(LoginUser loginUser, SqlCommand command, StringBuilder builder, TabularReport tabularReport, bool includeHiddenFields, bool isSchemaOnly, string sortField = null, string sortDir = null)
+        {
+            ReportSubcategory sub = ReportSubcategories.GetReportSubcategory(loginUser, tabularReport.Subcategory);
+
+            ReportTables tables = new ReportTables(loginUser);
+            tables.LoadAll();
+
+            ReportTableFields tableFields = new ReportTableFields(loginUser);
+            tableFields.LoadAll();
+            TimeSpan offset = loginUser.Offset;
+            TicketTypes ticketTypes = new TicketTypes(loginUser);
+            ticketTypes.LoadByOrganizationID(loginUser.OrganizationID);
+
+            string sortClause = "";
+
+
+            foreach (ReportSelectedField field in tabularReport.Fields)
+            {
+
+                if (field.IsCustom)
+                {
+                    CustomField customField = (CustomField)CustomFields.GetCustomField(loginUser, field.FieldID);
+                    if (customField == null) continue;
+                    string fieldName = DataUtils.GetReportPrimaryKeyFieldName(customField.RefType);
+                    if (fieldName != "")
+                    {
+                        //handle the ticket views custom fields
+                        if (tabularReport.Subcategory == 70)
+                        {
+                            fieldName = "UserTicketsView.TicketID";
+                        }
+                        else if (tabularReport.Subcategory == 74) fieldName = "TicketsView.TicketID";
+
+
+                        fieldName = DataUtils.GetCustomFieldColumn(loginUser, customField, fieldName, true, false);
+                        string colName = fieldName;
+
+                        if (customField.FieldType == CustomFieldType.DateTime)
+                        {
+                            fieldName = string.Format("CAST(SWITCHOFFSET(TODATETIMEOFFSET({0}, '+00:00'), '{1}{2:D2}:{3:D2}') AS DATETIME)",
+                            fieldName,
+                            offset < TimeSpan.Zero ? "-" : "+",
+                            Math.Abs(offset.Hours),
+                            Math.Abs(offset.Minutes));
+                        }
+                        else if (customField.FieldType == CustomFieldType.Boolean)
+                        {
+                            fieldName = string.Format("(SELECT ISNULL(({0}),0))", fieldName);
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(sortField) && colName == sortField)
+                        {
+                            sortClause = fieldName;
+                        }
+
+                        builder.Append(builder.Length < 1 ? "SELECT " : ", ");
+                        string displayName = customField.Name;
+                        if (customField.AuxID > 0 && customField.RefType == ReferenceType.Tickets)
+                        {
+                            TicketType ticketType = ticketTypes.FindByTicketTypeID(customField.AuxID);
+                            if (ticketType != null && ticketType.OrganizationID == customField.OrganizationID)
+                            {
+                                displayName = $"{customField.Name} ({ticketType.Name})";
+                            }
+                        }
+                        builder.Append($"{fieldName} AS [{displayName}]");
+
+                        if (!string.IsNullOrWhiteSpace(sortField) && displayName == sortField)
+                        {
+                            sortClause = fieldName;
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    ReportTableField tableField = tableFields.FindByReportTableFieldID(field.FieldID);
+
+                    ReportTable table = tables.FindByReportTableID(tableField.ReportTableID);
+                    string fieldName = table.TableName + "." + tableField.FieldName;
+                    if (tableField.DataType.Trim().ToLower() == "datetime")
+                    {
+                        fieldName = string.Format("CAST(SWITCHOFFSET(TODATETIMEOFFSET({0}, '+00:00'), '{1}{2:D2}:{3:D2}') AS DATETIME)",
+                          fieldName,
+                          offset < TimeSpan.Zero ? "-" : "+",
+                          Math.Abs(offset.Hours),
+                          Math.Abs(offset.Minutes));
+
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(sortField) && tableField.Alias == sortField)
+                    {
+                        sortClause = fieldName;
+                    }
+
+                    if (builder.Length < 1)
+                    {
+                        builder.Append("SELECT " + fieldName + " AS [" + tableField.Alias + "]");
+                    }
+                    else
+                    {
+                        builder.Append(", " + fieldName + " AS [" + tableField.Alias + "]");
+                    }
+
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(sortClause))
+            {
+               builder.Append($", ROW_NUMBER() OVER (ORDER BY {sortClause} {sortDir}) AS [RowNum]");
+            }
+
+            if (includeHiddenFields)
+            {
+                ReportTable hiddenTable = tables.FindByReportTableID(sub.ReportCategoryTableID);
+                if (!string.IsNullOrWhiteSpace(hiddenTable.LookupKeyFieldName))
+                {
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", hiddenTable.LookupKeyFieldName, hiddenTable.TableName));
+                }
+
+                if (sub.ReportTableID != null)
+                {
+                    hiddenTable = tables.FindByReportTableID((int)sub.ReportTableID);
+                    if (!string.IsNullOrWhiteSpace(hiddenTable.LookupKeyFieldName))
+                        builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", hiddenTable.LookupKeyFieldName, hiddenTable.TableName));
+                }
+
+                if (tabularReport.Subcategory == 70)
+                {
+                    string dueDateField = hiddenTable.TableName + ".DueDate";
+                    dueDateField = string.Format("CAST(SWITCHOFFSET(TODATETIMEOFFSET({0}, '+00:00'), '{1}{2:D2}:{3:D2}') AS DATETIME)",
+                    dueDateField,
+                    offset < TimeSpan.Zero ? "-" : "+",
+                    Math.Abs(offset.Hours),
+                    Math.Abs(offset.Minutes));
+                    builder.Append(string.Format(", {0} AS [hiddenDueDate]", dueDateField));
+
+                    string dateModifiedField = hiddenTable.TableName + ".DateModified";
+                    dateModifiedField = string.Format("CAST(SWITCHOFFSET(TODATETIMEOFFSET({0}, '+00:00'), '{1}{2:D2}:{3:D2}') AS DATETIME)",
+                    dateModifiedField,
+                    offset < TimeSpan.Zero ? "-" : "+",
+                    Math.Abs(offset.Hours),
+                    Math.Abs(offset.Minutes));
+                    builder.Append(string.Format(", {0} AS [hiddenDateModified]", dateModifiedField));
+
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", "SlaWarningTime", hiddenTable.TableName));
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", "SlaViolationTime", hiddenTable.TableName));
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", "IsRead", hiddenTable.TableName));
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", "IsClosed", hiddenTable.TableName));
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", "TicketTypeID", hiddenTable.TableName));
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", "UserID", hiddenTable.TableName));
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", "SeverityPosition", hiddenTable.TableName));
+                    builder.Append(string.Format(", {1}.{0} AS [hidden{0}]", "StatusPosition", hiddenTable.TableName));
+                }
+
+            }
+            builder.Append(" " + sub.BaseQuery);
+
+            ReportTable mainTable = tables.FindByReportTableID(sub.ReportCategoryTableID);
+            builder.Append(" WHERE (" + mainTable.TableName + "." + mainTable.OrganizationIDFieldName + " = @OrganizationID)");
+            if (tabularReport.Subcategory == 70)
+            {
+                builder.Append(" AND (" + mainTable.TableName + ".ViewerID = @UserID)");
+            }
+
+            UseTicketRights(loginUser, (int)tabularReport.Subcategory, tables, command, builder);
+
+            if (isSchemaOnly) builder.Append(" AND (0=1)");
+        }
+
 
         private static void UseTicketRights(LoginUser loginUser, int subCAtID, ReportTables tables, SqlCommand command, StringBuilder builder)
         {
@@ -2545,7 +2716,9 @@ WHERE RowNum BETWEEN @From AND @To";
             }
 
             //On exports it is always false
-            //if (!includeHiddenFields) table.Columns.Remove("RowNum");
+            //if (!includeHiddenFields) 
+            if (table.Columns.Contains("RowNum"))
+                table.Columns.Remove("RowNum");
 
             return table;
         }
